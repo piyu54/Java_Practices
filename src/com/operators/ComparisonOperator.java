b2b455b8788f8562d442837b6bf7ea820c118e67
piyu54 <priyakushwaha1407@gmail.com>
2025-11-05 01:06:33 +0530

    commit

---
+++ src/com/operators/ComparisonOperator.java
@@ -0,0 +1,20 @@
+package com.operators;
+
+public class ComparisonOperator {
+
+	public static void main(String[] args) {
+		
+//		>,<,>=,<=,==,!=
+		
+		int a=9;
+		int b=19;
+		System.out.println("---------------Relation / Comparsion Operator");
+		System.out.println(a+" is greater than "+b+ " "+(a>b));
+		System.out.println(a+" is smaller than "+b+" "+(a<b));
+		System.out.println(a+" is greater than or equal to "+b+" "+(a>=b));
+		System.out.println(a+" is smaller than or equal to "+b+" "+(a<=b));
+		System.out.println(a+" is equals to "+b+" "+(a==b));
+		System.out.println(a+" is not equals to  "+b+" "+(a!=b));
+	}
+
+}
