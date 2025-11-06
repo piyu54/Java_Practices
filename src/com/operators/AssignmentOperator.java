e0238672c2bc548b5059e49021fc43084e1c321e
piyu54 <priyakushwaha1407@gmail.com>
2025-11-06 17:08:59 +0530

    Assignment operator

---
+++ src/com/operators/AssignmentOperator.java
@@ -0,0 +1,22 @@
+package com.operators;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		System.out.println("----------Assignment operator-----------");
+		
+		int a=5;
+		int b=9;
+		
+		System.out.println(a+=b);//addition a+b=14
+		System.out.println(a-=b);//sub a-b= 14-9=5
+		System.out.println(a/=b);
+		System.out.println(a*=b);
+		System.out.println(a%=b);
+		
+		
+		
+
+	}
+
+}
