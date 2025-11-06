9ef8b5dbc4f87626281ac63ceed1e085d85e6343
piyu54 <priyakushwaha1407@gmail.com>
2025-11-06 17:14:55 +0530

    commit

--- src/com/operators/AssignmentOperator.java
+++ src/com/operators/AssignmentOperator.java
@@ -8,6 +8,7 @@ public class AssignmentOperator {
 		int a=5;
 		int b=9;
 		
+		System.out.println("= Assignment operator");
 		System.out.println(a+=b);//addition a+b=14
 		System.out.println(a-=b);//sub a-b= 14-9=5
 		System.out.println(a/=b);
