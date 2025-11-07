48f48fd05db01d3840bcc2288683ac9c1e2e8438
piyu54 <priyakushwaha1407@gmail.com>
2025-11-07 18:08:13 +0530

    commit

--- src/com/operators/BitwiseOperators.java
+++ src/com/operators/BitwiseOperators.java
@@ -5,13 +5,22 @@ public class BitwiseOperators {
 	public static void main(String[] args) {
 		// Bitwise Operators
 		
-		// ...512 256 128 64 32 16 8 4 2 1
+		/* ...512 256 128 64 32 16 8 4 2 1
+		 bitwise operator works bit by bit
+		 
+		 denote by & true(1)& false(0)
+		 */
 
 		System.out.println("---------Bitwise Operator----------");
 		System.out.println("Decimal to Binary");
 		int a=125;
 		
 		System.out.println(Integer.toBinaryString(a));
+		
+		System.out.println("Operation with &");
+		
+		System.out.println(4&23);
+		System.out.println(14&43);
 	}
 
 }
