435a75ced741e51c91dd5b8b0c074cf549b85c2b
piyu54 <priyakushwaha1407@gmail.com>
2025-11-07 17:34:22 +0530

    commit

---
+++ src/com/operators/BitwiseOperators.java
@@ -0,0 +1,13 @@
+package com.operators;
+
+public class BitwiseOperators {
+
+	public static void main(String[] args) {
+		// Bitwise Operators
+		
+		// ...512 256 128 64 32 16 8 4 2 1
+
+		System.out.println("---------Bitwise Operator----------");
+	}
+
+}
