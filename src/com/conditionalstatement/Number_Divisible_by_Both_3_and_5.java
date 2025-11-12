762524c9d96ed36027fe98204a399d1507bccb41
piyu54 <priyakushwaha1407@gmail.com>
2025-11-13 00:08:44 +0530

    commit

---
+++ src/com/conditionalstatement/Number_Divisible_by_Both_3_and_5.java
@@ -0,0 +1,19 @@
+package com.conditionalstatement;
+
+public class Number_Divisible_by_Both_3_and_5 {
+
+	public static void main(String[] args) {
+		int num=15;
+		
+		if(num%3==0 && num%5==0)
+		{
+			System.out.println("this number is Divisible by Both 3 and 5");
+		}
+		else
+		{
+			System.out.println("this number does not Divisible_by_Both_3_and_5");
+		}
+
+	}
+
+}
