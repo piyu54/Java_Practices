34995498b80168e1bd1a40ab76a36d05b0d2daeb
piyu54 <priyakushwaha1407@gmail.com>
2025-11-12 23:41:54 +0530

    greater of 2 num

---
+++ src/com/conditionalstatement/Greater_Of_Two_Num.java
@@ -0,0 +1,20 @@
+package com.conditionalstatement;
+
+public class Greater_Of_Two_Num {
+
+	public static void main(String[] args) {
+		int a=28;
+		int b=19;
+		
+		if(a>b)//28>19 true
+		{
+			System.out.println("a is an greater");
+		}
+		else
+		{
+			System.out.println("b is greateer");
+		}
+
+	}
+
+}
