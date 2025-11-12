49ebb7576b89fa4d318dad0b2b31c8787f39f15f
piyu54 <priyakushwaha1407@gmail.com>
2025-11-12 23:34:33 +0530

    commit

---
+++ src/com/conditionalstatement/Even_Odd.java
@@ -0,0 +1,19 @@
+package com.conditionalstatement;
+
+public class Even_Odd {
+
+	public static void main(String[] args) {
+		int num = 4;
+		
+		if(num%2==0)
+		{
+			System.out.println("this is an even num");
+		}
+		else
+		{
+			System.out.println("this is an odd num");
+		}
+
+	}
+
+}
