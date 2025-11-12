33b4fd801e2607c568b852939a97619bd7e8ba69
piyu54 <priyakushwaha1407@gmail.com>
2025-11-12 23:30:19 +0530

    commit

--- src/com/conditionalstatement/Nested_if.java
+++ src/com/conditionalstatement/Nested_if.java
@@ -16,6 +16,34 @@ public class Nested_if {
 			System.out.println("invaild");
 		}
 		//hie is execute
+		
+		
+		if(false)
+		{
+			if(true)
+			{
+				System.out.println("Hie");
+			}
+			
+		}
+		else
+		{
+			System.out.println("invaild");
+		}
+		//here  invalid execute
+		if(true)
+		{
+			if(false)
+			{
+				System.out.println("Hie");
+			}
+			
+		}
+		else
+		{
+			System.out.println("invaild");
+		}
+		//here blank 
 	}
 
 }
