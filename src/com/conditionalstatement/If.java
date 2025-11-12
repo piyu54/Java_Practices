31e690adc1a1f249c49879c0d0ad22a0be4593d5
piyu54 <priyakushwaha1407@gmail.com>
2025-11-12 20:59:37 +0530

    commit

--- src/com/conditionalstatement/If.java
+++ src/com/conditionalstatement/If.java
@@ -8,6 +8,21 @@ public class If {
 			System.out.println("You can vote");
 		}
 
+		
+		int age=24;
+		
+		if(age>=18)
+		{
+			System.out.println("you can vote 1!");
+		}
+		
+		
+		int age2=5;
+		
+		if(age2>=18) //here the consition is false thats why if block didnt execute
+		{
+			System.out.println("you can vote 2");
+		}
 	}
 
 }
