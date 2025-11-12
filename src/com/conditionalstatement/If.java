741be34d0f95b85364c802df0df8228563a28988
piyu54 <priyakushwaha1407@gmail.com>
2025-11-12 20:56:02 +0530

    commit

--- src/com/conditionalstatement/If.java
+++ src/com/conditionalstatement/If.java
@@ -3,7 +3,7 @@ package com.conditionalstatement;
 public class If {
 
 	public static void main(String[] args) {
-		if(22>18)
+		if(22>18) //if block execute only when the condition is true
 		{
 			System.out.println("You can vote");
 		}
