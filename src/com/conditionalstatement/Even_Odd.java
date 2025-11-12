e1b4bf4788871b3e75d801452def2d9b7d74d444
piyu54 <priyakushwaha1407@gmail.com>
2025-11-12 23:37:52 +0530

    commit

--- src/com/conditionalstatement/Even_Odd.java
+++ src/com/conditionalstatement/Even_Odd.java
@@ -5,6 +5,7 @@ public class Even_Odd {
 	public static void main(String[] args) {
 		int num = 4;
 		
+		//num %2==0 means koi num jo 2 se divide ho and rem 0 aaye wo even h baki odd
 		if(num%2==0)
 		{
 			System.out.println("this is an even num");
