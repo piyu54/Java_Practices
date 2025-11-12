d6479ba62864ff3237b850d088a4ab8d693c9a2b
piyu54 <priyakushwaha1407@gmail.com>
2025-11-12 21:05:07 +0530

    commit

--- src/com/conditionalstatement/If_else.java
+++ src/com/conditionalstatement/If_else.java
@@ -3,7 +3,16 @@ package com.conditionalstatement;
 public class If_else {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int age = 19;
+		
+		//if execute when condition is true and else block execute when consition is false
+		if (age >= 18) {
+			System.out.println("you can vote");
+		}
+		else
+		{
+			System.out.println("you cant vote first be an 18+");
+		}
 
 	}
 
--- src/com/conditionalstatement/If_else_if.java
+++ src/com/conditionalstatement/If_else_if.java
@@ -5,11 +5,11 @@ public class If_else_if {
 	public static void main(String[] args) {
 		int age = 19;
 
+		/*if execute when condition is true and else block execute when condition is
+		 false but  when it comes to an if else if */
 		if (age >= 18) {
 			System.out.println("you can vote");
-		}
-		else
-		{
+		} else {
 			System.out.println("you cant vote first be an 18+");
 		}
 
