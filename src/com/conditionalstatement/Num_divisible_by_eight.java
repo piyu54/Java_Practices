b4a25a42da9099a86050803080b5d8283b1ebeea
piyu54 <priyakushwaha1407@gmail.com>
2025-11-13 17:23:31 +0530

    commit

---
+++ src/com/conditionalstatement/Num_divisible_by_eight.java
@@ -0,0 +1,20 @@
+package com.conditionalstatement;
+
+public class Num_divisible_by_eight {
+
+	public static void main(String[] args) {
+		//Write a program to check whether a number is divisible by 10.
+		
+		int num=60;
+		
+		if(num%10==0)
+		{
+			System.out.println("this num is divisible by 10");
+		}
+		else
+		{
+			System.out.println("this num is not divisible by 10");
+		}
+	}
+
+}
