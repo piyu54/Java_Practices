a3faba4f7f660e64f030718d83934e607c86757b
piyu54 <priyakushwaha1407@gmail.com>
2025-11-13 17:42:01 +0530

    Commit

---
+++ src/com/conditionalstatement/Week_day.java
@@ -0,0 +1,41 @@
+package com.conditionalstatement;
+
+public class Week_day {
+
+	public static void main(String[] args) {
+		// Write a program to print the day of the week based on day number (1–7).
+		
+		
+		int day=6;
+		
+		if(day==1)
+		{
+			System.out.println("Monday");
+		}
+		else if(day==2)
+		{
+			System.out.println("Tuesday");
+		}
+		else if(day==3)
+		{
+			System.out.println("Wednesday");
+		}
+		else if(day==4)
+		{
+			System.out.println("Thursday");
+		}
+		else if(day==5)
+		{
+			System.out.println("Friday");
+		}
+		else if(day==6)
+		{
+			System.out.println("Saturday");
+		}
+		else if(day==7)
+		{
+			System.out.println("Sunday");
+		}
+	}
+
+}
