6f94c95cbf7d0bf9ccfa5b6bbf7809b506a20d9c
piyu54 <priyakushwaha1407@gmail.com>
2025-11-13 17:21:04 +0530

    commit

---
+++ src/com/conditionalstatement/Person_age.java
@@ -0,0 +1,20 @@
+package com.conditionalstatement;
+
+public class Person_age {
+
+	public static void main(String[] args) {
+		//Write a program that checks if a person’s age is greater than 18, then print "Adult".
+		
+		int age=18;
+		if(age>=18)
+		{
+			System.out.println("Adult");
+		}
+		else
+		{
+			System.out.println("Not an Adult");
+		}
+
+	}
+
+}
