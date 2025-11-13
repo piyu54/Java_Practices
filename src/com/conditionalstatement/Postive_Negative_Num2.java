01eab59ae00a8430485ac3acaebad95ece4d629d
piyu54 <priyakushwaha1407@gmail.com>
2025-11-13 18:04:16 +0530

    commit

---
+++ src/com/conditionalstatement/Postive_Negative_Num2.java
@@ -0,0 +1,23 @@
+package com.conditionalstatement;
+
+public class Postive_Negative_Num2 {
+	//Write a program to check whether a number is positive, negative, or zero.
+	public static void main(String[] args) {
+		int num =9;
+		
+		if(num>0)
+		{
+			System.out.println("this is an postive number");
+		}
+		else if (num<0)
+		{
+			System.out.println("this is an negative number");
+		}
+		else
+		{
+			System.out.println("this is an zero");
+		}
+
+	}
+
+}
