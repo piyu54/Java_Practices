67b20cdcbe8e6df56c577cb6bb0c0611f7e4ff8a
piyu54 <priyakushwaha1407@gmail.com>
2025-11-13 17:27:32 +0530

    commit

---
+++ src/com/conditionalstatement/Check_Temperature.java
@@ -0,0 +1,22 @@
+package com.conditionalstatement;
+
+public class Check_Temperature {
+
+	public static void main(String[] args) {
+		// Write a program to check whether a temperature is above 30°C, then print "Hot Day".
+		
+		
+		int temperature = 20;
+		
+		if(temperature>=30)
+		{
+			System.out.println("Hot day");
+		}
+		else
+		{
+			System.out.println("Normal Day");
+		}
+
+	}
+
+}
