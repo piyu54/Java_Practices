b274afc7fe326a614d0c2b595f4d99ee43e4c56c
piyu54 <priyakushwaha1407@gmail.com>
2025-11-15 23:35:56 +0530

    commit

--- src/com/conditionalstatement/AllExamples.java
+++ src/com/conditionalstatement/AllExamples.java
@@ -29,6 +29,21 @@ public class AllExamples {
 		} else {
 		  System.out.println("Not old enough to vote.");
 		}
+		
+		int age = 20;
+		boolean isCitizen = true;
+
+		if (age >= 18) {
+		  System.out.println("Old enough to vote.");
+		  
+		  if (isCitizen) {
+		    System.out.println("And you are a citizen, so you can vote!");
+		  } else {
+		    System.out.println("But you must be a citizen to vote.");
+		  }
+		} else {
+		  System.out.println("Not old enough to vote.");
+		}
 
 	}
 
