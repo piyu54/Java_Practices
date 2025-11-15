aa8e69183db2468e43d0896d9979d78963090499
piyu54 <priyakushwaha1407@gmail.com>
2025-11-15 23:26:28 +0530

    commit

--- src/com/conditionalstatement/If_else.java
+++ src/com/conditionalstatement/If_else.java
@@ -14,6 +14,22 @@ public class If_else {
 			System.out.println("you cant vote first be an 18+");
 		}
 
+		boolean isRaining = false;
+
+		if (isRaining) {
+		  System.out.println("Bring an umbrella!");
+		} else {
+		  System.out.println("No rain today, no need for an umbrella!");
+		}
+		
+		int time = 20;
+
+		if (time < 18) {
+		  System.out.println("Good day.");
+		} else {
+		  System.out.println("Good evening.");
+		}
+		// Outputs "Good evening."
 	}
 
 }
