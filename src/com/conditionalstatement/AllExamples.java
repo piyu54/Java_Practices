e0443a0ece04cbaaa7995610e25183d564cc5d57
piyu54 <priyakushwaha1407@gmail.com>
2025-11-15 23:36:58 +0530

    commit

--- src/com/conditionalstatement/AllExamples.java
+++ src/com/conditionalstatement/AllExamples.java
@@ -53,7 +53,15 @@ public class AllExamples {
 		} else {
 		  System.out.println(myNum2 + " is odd");
 		} 
+		int temperature = 30;
 
+		if (temperature < 0) {
+		  System.out.println("It's freezing!");
+		} else if (temperature < 20) {
+		  System.out.println("It's cool.");
+		} else {
+		  System.out.println("It's warm.");
+		} 
 	}
 
 }
