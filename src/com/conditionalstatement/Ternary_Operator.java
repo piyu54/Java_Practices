169bbb5c3f73353944a4060d13ff221e2c7dc58f
piyu54 <priyakushwaha1407@gmail.com>
2025-11-15 23:30:45 +0530

    commit

---
+++ src/com/conditionalstatement/Ternary_Operator.java
@@ -0,0 +1,12 @@
+package com.conditionalstatement;
+
+public class Ternary_Operator {
+
+	public static void main(String[] args) {
+		int time = 20;
+		String result = (time < 18) ? "Good day." : "Good evening.";
+		System.out.println(result);
+
+	}
+
+}
