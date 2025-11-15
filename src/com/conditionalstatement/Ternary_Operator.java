f4e178f6c80b65b43f951dba9cbfa375222260c6
piyu54 <priyakushwaha1407@gmail.com>
2025-11-15 23:31:54 +0530

    commit

--- src/com/conditionalstatement/Ternary_Operator.java
+++ src/com/conditionalstatement/Ternary_Operator.java
@@ -3,10 +3,17 @@ package com.conditionalstatement;
 public class Ternary_Operator {
 
 	public static void main(String[] args) {
-		int time = 20;
-		String result = (time < 18) ? "Good day." : "Good evening.";
-		System.out.println(result);
-
+//		int time = 20;
+//		String result = (time < 18) ? "Good day." : "Good evening.";
+//		System.out.println(result);
+//		int time2 = 20;
+//		System.out.println((time2 < 18) ? "Good day." : "Good evening.");
+		
+		int time = 22;
+		String message = (time < 12) ? "Good morning."
+		               : (time < 18) ? "Good afternoon."
+		               : "Good evening.";
+		System.out.println(message);
 	}
 
 }
