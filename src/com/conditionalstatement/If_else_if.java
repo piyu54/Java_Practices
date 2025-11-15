6a631ad0e6fc1f57f4162da2021843dffa815942
piyu54 <priyakushwaha1407@gmail.com>
2025-11-15 23:29:47 +0530

    commit

--- src/com/conditionalstatement/If_else_if.java
+++ src/com/conditionalstatement/If_else_if.java
@@ -68,6 +68,16 @@ public class If_else_if {
 		  System.out.println("Just go outside normally.");
 		}
 		// Outputs "Wear sunglasses."
+		
+		int time = 22;
+		if (time < 10) {
+		  System.out.println("Good morning.");
+		} else if (time < 18) {
+		  System.out.println("Good day.");
+		} else {
+		  System.out.println("Good evening.");
+		}
+		// Outputs "Good evening."
 	}
 
 }
