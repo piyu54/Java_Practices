ccfab000271414e9ee4163e2671312fe0b32e48b
piyu54 <priyakushwaha1407@gmail.com>
2025-11-15 23:26:20 +0530

    commit

--- src/com/conditionalstatement/If_else_if.java
+++ src/com/conditionalstatement/If_else_if.java
@@ -56,6 +56,18 @@ public class If_else_if {
 			System.out.println("you cant vote first be an 18+");
 		}
 		//here else if block execute
+		
+		
+		int weather = 2; // 1 = raining, 2 = sunny, 3 = cloudy
+
+		if (weather == 1) {
+		  System.out.println("Bring an umbrella.");
+		} else if (weather == 2) {
+		  System.out.println("Wear sunglasses.");
+		} else {
+		  System.out.println("Just go outside normally.");
+		}
+		// Outputs "Wear sunglasses."
 	}
 
 }
