0f2f1f850a5402525b870c00849055cb47e1160b
piyu54 <priyakushwaha1407@gmail.com>
2025-11-17 00:22:19 +0530

    commit

--- src/com/operatorsQuestions/Example10.java
+++ src/com/operatorsQuestions/Example10.java
@@ -3,7 +3,16 @@ package com.operatorsQuestions;
 public class Example10 {
 
 	public static void main(String[] args) {
+		int a = 4, b = 2;
+		System.out.println(a << b);
 		
+		
+		/*
+		 * 16 8 4 2 1 
+		 * 0  0 1 0	0
+		 * a<<b
+		 * 1  0 0 0 0
+		 * */
 
 	}
 
