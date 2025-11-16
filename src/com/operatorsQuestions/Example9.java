ddea99a116acb8852589a2da7a33c2985d9cf7b3
piyu54 <priyakushwaha1407@gmail.com>
2025-11-17 00:20:13 +0530

    commit

--- src/com/operatorsQuestions/Example9.java
+++ src/com/operatorsQuestions/Example9.java
@@ -3,8 +3,10 @@ package com.operatorsQuestions;
 public class Example9 {
 
 	public static void main(String[] args) {
-		
 
+		int a = 10, b = 5, c;
+		c = a++ + --b + a * b;
+		System.out.println(c);
 	}
 
 }
