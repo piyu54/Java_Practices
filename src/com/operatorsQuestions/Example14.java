b5b4c59f247f8761887696ad38cbb80052976e7d
piyu54 <priyakushwaha1407@gmail.com>
2025-11-17 00:29:12 +0530

    commit

--- src/com/operatorsQuestions/Example14.java
+++ src/com/operatorsQuestions/Example14.java
@@ -3,7 +3,26 @@ package com.operatorsQuestions;
 public class Example14 {
 
 	public static void main(String[] args) {
+		int a = 5, b = 7;
+		System.out.println(a | b);
+
 		
+		/*
+		 * 5 
+		 * 4 2 1
+		 * 1 0 1
+		 * 
+		 * 7 
+		 * 4 2 1
+		 * 1 1 1
+		 * 
+		 * 5+7 OR case
+		 * 101
+		 * 111
+		 * -----
+		 * 111=7
+		 * 421
+		 * */
 
 	}
 
