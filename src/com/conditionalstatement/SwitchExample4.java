a7960ed27b52b2aae4fc87f5605436db8726f06c
piyu54 <priyakushwaha1407@gmail.com>
2025-11-17 17:43:50 +0530

    commit

---
+++ src/com/conditionalstatement/SwitchExample4.java
@@ -0,0 +1,29 @@
+package com.conditionalstatement;
+
+public class SwitchExample4 {
+
+	public static void main(String[] args) {
+
+		String name = "sakshue";
+
+		switch (name) {
+		case "piyu":
+			System.out.println("my name is piyu");
+			break;
+
+		case "sakshu":
+			System.out.println("my name is sakshu");
+			break;
+
+		case "pritu":
+			System.out.println("my name is pritu");
+			break;
+
+		default:
+			System.out.println("you enter wrong name");
+			break;
+
+		}
+	}
+
+}
