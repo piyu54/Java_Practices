0dfde24c0138a7e43361bad28c783e9dc2e5614f
piyu54 <priyakushwaha1407@gmail.com>
2025-11-17 22:59:23 +0530

    commit

--- src/com/conditionalstatement/SwitchExample12.java
+++ src/com/conditionalstatement/SwitchExample12.java
@@ -4,7 +4,27 @@ public class SwitchExample12 {
 //8. Accept a category type (Domestic, Commercial, Industrial) and print the electricity rate per unit using a switch case.
 	
 	public static void main(String[] args) {
+		String elerate="Domestic";
 		
+		switch(elerate)
+		{
+		
+		case "Domestic":
+		System.out.println("The electricity rate per unit 1200");
+		break;
+		
+		case "Commercial":
+			System.out.println("The electricity rate per unit 2000");
+			break;
+			
+		case "Industrial":
+			System.out.println("The electricity rate per unit 3000");
+			break;
+			
+		default:
+			System.out.println("Invaild input");
+			
+		}
 		
 	}
 }
