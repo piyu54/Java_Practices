bc4677ca166887a9719b8dc9294d8fbf4ffa6495
piyu54 <priyakushwaha1407@gmail.com>
2025-11-17 17:38:28 +0530

    commit

---
+++ src/com/conditionalstatement/SwitchExample3.java
@@ -0,0 +1,23 @@
+package com.conditionalstatement;
+
+public class SwitchExample3 {
+
+	public static void main(String[] args) {
+		int num = 2;
+		switch (num) {
+		case 1:
+			System.out.println("samosa");
+
+			break;
+		case 2:
+			System.out.println("mango");
+			break;
+
+		default:
+			System.out.println("We wake up");
+			break;
+		}
+
+	}
+
+}
