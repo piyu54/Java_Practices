00dfc751f4b0b73460ae3452393f113113623644
piyu54 <priyakushwaha1407@gmail.com>
2025-11-17 17:48:42 +0530

    commit

---
+++ src/com/conditionalstatement/SwitchExample5.java
@@ -0,0 +1,29 @@
+package com.conditionalstatement;
+
+public class SwitchExample5 {
+	public static void main(String[] args) {
+
+		char firstletter = 'V';
+
+		switch (firstletter) {
+
+		case 'P':
+			System.out.println("This first char is P");
+			break;
+
+		case 'V':
+			System.out.println("This first char is V");
+			break;
+
+		case 'S':
+			System.out.println("This first char is S");
+			break;
+		case 'I':
+			System.out.println("This first char is I");
+			break;
+		default:
+			System.out.println("Enter right first letter ");
+			break;
+		}
+	}
+}
