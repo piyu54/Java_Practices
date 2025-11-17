35a54d5e665c25ac1b4d5f53ad09eaea3850e680
piyu54 <priyakushwaha1407@gmail.com>
2025-11-17 23:06:18 +0530

    commit

--- src/com/conditionalstatement/SwitchExample10.java
+++ src/com/conditionalstatement/SwitchExample10.java
@@ -2,4 +2,35 @@ package com.conditionalstatement;
 
 public class SwitchExample10 {
 //6. Take a single character input and check whether it is a vowel or consonant using a switch statement.
+
+	public static void main(String[] args) {
+
+		// a,e,i,o,u
+
+		char vowel = 'p';
+
+		switch (vowel) {
+
+		case 'a':
+			System.out.println("This is an vowel");
+			break;
+
+		case 'e':
+			System.out.println("This is an vowel");
+			break;
+		case 'i':
+			System.out.println("This is an vowel");
+			break;
+		case 'o':
+			System.out.println("This is an vowel");
+			break;
+		case 'u':
+			System.out.println("This is an vowel");
+			break;
+		default:
+			System.out.println("This is not an vowel");
+
+		}
+	}
+
 }
--- src/com/conditionalstatement/SwitchExample11.java
+++ src/com/conditionalstatement/SwitchExample11.java
@@ -2,15 +2,20 @@ package com.conditionalstatement;
 
 public class SwitchExample11 {
 //7. Use a switch statement to check if a number is even or odd (hint: use number % 2 as the expression)
-	
+
 	public static void main(String[] args) {
-		
-		int num=6;
-		
-		switch(num%2)
-		{
+
+		int num = 6;
+
+		switch (num % 2) {
+		case 0:
+			System.out.println(num + " is Even");
+			break;
+
 		case 1:
-			
+			System.out.println(num + " is Odd");
+			break;
+
 		}
 	}
 }
