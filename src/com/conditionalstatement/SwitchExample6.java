9c9446fce364d2791e07e26d40d8272d50a37a47
piyu54 <priyakushwaha1407@gmail.com>
2025-11-17 18:06:38 +0530

    calculator

--- src/com/conditionalstatement/SwitchExample6.java
+++ src/com/conditionalstatement/SwitchExample6.java
@@ -2,7 +2,29 @@ package com.conditionalstatement;
 
 public class SwitchExample6 {
 	public static void main(String[] args) {
-		
-		
+
+		char operation = '*';
+
+		switch (operation) {
+		case '+':
+			System.out.println("Addition of two num " + (2 + 2));
+			break;
+
+		case '-':
+			System.out.println("Sub of two num " + (2 - 2));
+			break;
+		case '*':
+			System.out.println("Multi of two num " + (2 * 2));
+			break;
+		case '/':
+			System.out.println("Div of two num " + (2 / 2));
+			break;
+		case '%':
+			System.out.println("Mod of two num " + (2 % 2));
+			break;
+		default:
+			System.out.println("Invaild operation");
+
+		}
 	}
 }
