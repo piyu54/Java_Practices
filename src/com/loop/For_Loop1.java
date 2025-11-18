248dcd624f8c98cd8a644ea99eb37d0b4360cdf5
piyu54 <priyakushwaha1407@gmail.com>
2025-11-18 19:50:03 +0530

    commit

---
+++ src/com/loop/For_Loop1.java
@@ -0,0 +1,14 @@
+package com.loop;
+
+public class For_Loop1 {
+
+	public static void main(String[] args) {
+		// Print numbers from 1 to 10 using a loop.
+		
+		for(int i =1;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
