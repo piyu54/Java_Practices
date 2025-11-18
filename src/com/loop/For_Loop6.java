16786d733356709623c34c625c305beaf8a72118
piyu54 <priyakushwaha1407@gmail.com>
2025-11-18 20:10:02 +0530

    commit

---
+++ src/com/loop/For_Loop6.java
@@ -0,0 +1,17 @@
+package com.loop;
+
+public class For_Loop6 {
+
+	public static void main(String[] args) {
+		// Print the sum of first 10 natural numbers.
+
+		int sum=0;
+		
+		for(int i=1;i<=10;i++)
+		{
+			sum+=i;
+		}
+		System.out.println("Sum of first 10 natural numbers: "+sum);
+	}
+
+}
