f7489f591c94a58f36a89086c267b8a5eadaf07a
piyu54 <priyakushwaha1407@gmail.com>
2025-11-18 23:47:26 +0530

    commit

---
+++ src/com/loop/While_Loop6.java
@@ -0,0 +1,15 @@
+package com.loop;
+
+public class While_Loop6 {
+
+	public static void main(String[] args) {
+		// 
+		int i = 1;
+		while(i <= 5) {
+		    System.out.println(i);
+		    i++;
+		}
+
+	}
+
+}
