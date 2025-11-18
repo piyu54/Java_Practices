26773d04cdfabc63fa0ad248641d60ecde46a9b7
piyu54 <priyakushwaha1407@gmail.com>
2025-11-18 20:01:16 +0530

    commit

---
+++ src/com/loop/For_Loop3.java
@@ -0,0 +1,18 @@
+package com.loop;
+
+public class For_Loop3 {
+
+	public static void main(String[] args) {
+//		Print the table of 5 using a loop.
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i*5);
+		}
+		
+		
+	}
+	}
+
+
+
