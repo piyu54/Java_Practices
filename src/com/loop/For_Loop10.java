79e84cf23915268aa93f875065599e70c3baa69b
piyu54 <priyakushwaha1407@gmail.com>
2025-11-18 20:19:24 +0530

    commit

---
+++ src/com/loop/For_Loop10.java
@@ -0,0 +1,10 @@
+package com.loop;
+
+public class For_Loop10 {
+
+	public static void main(String[] args) {
+		// Print all odd numbers between 1 and 50.
+
+	}
+
+}
---
+++ src/com/loop/For_Loop9.java
@@ -0,0 +1,15 @@
+package com.loop;
+
+public class For_Loop9 {
+
+	public static void main(String[] args) {
+		// Print all even numbers between 1 and 50.
+		
+		for(int i=2;i<=50;i+=2)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
