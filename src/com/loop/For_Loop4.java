0cce121405ceaa47dbd98699bb6edba1d9050f67
piyu54 <priyakushwaha1407@gmail.com>
2025-11-18 20:03:21 +0530

    commit

---
+++ src/com/loop/For_Loop4.java
@@ -0,0 +1,15 @@
+package com.loop;
+
+public class For_Loop4 {
+
+	public static void main(String[] args) {
+		// Print the table of any number (e.g., n = 7).
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(i*2);
+		}
+
+	}
+
+}
