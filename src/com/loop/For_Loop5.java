6176ed3faa162d026310c613ec59a688cf08d2db
piyu54 <priyakushwaha1407@gmail.com>
2025-11-18 20:05:53 +0530

    commit

---
+++ src/com/loop/For_Loop5.java
@@ -0,0 +1,15 @@
+package com.loop;
+
+public class For_Loop5 {
+
+	public static void main(String[] args) {
+//		Print Hello 10 times.
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println("Hello = "+i);
+		}
+
+	}
+
+}
