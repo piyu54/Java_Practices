77344e4b28349add513ed93ad641853109038d2e
piyu54 <priyakushwaha1407@gmail.com>
2025-11-18 20:13:07 +0530

    commit

---
+++ src/com/loop/For_Loop7.java
@@ -0,0 +1,19 @@
+package com.loop;
+
+public class For_Loop7 {
+
+	public static void main(String[] args) {
+		// Print the sum of even numbers from 1 to 100.
+		
+		
+		int sum=0;
+		
+		for(int i=2;i<=100;i+=2)
+		{
+			sum+=i;
+		}
+		System.out.println("Sum of first 10 natural numbers: "+sum);
+
+	}
+
+}
