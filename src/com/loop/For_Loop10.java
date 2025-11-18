f901f5071e10e8a06d8def01b2d131094678f693
piyu54 <priyakushwaha1407@gmail.com>
2025-11-18 20:19:47 +0530

    commit

--- src/com/loop/For_Loop10.java
+++ src/com/loop/For_Loop10.java
@@ -4,6 +4,11 @@ public class For_Loop10 {
 
 	public static void main(String[] args) {
 		// Print all odd numbers between 1 and 50.
+		
+		for(int i=1;i<=50;i+=2)
+		{
+			System.out.println(i);
+		}
 
 	}
 
