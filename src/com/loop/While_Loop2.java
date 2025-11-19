e16a7e075a8af05913a9e271796eb610f6229e74
piyu54 <priyakushwaha1407@gmail.com>
2025-11-19 22:22:50 +0530

    commit

--- src/com/loop/While_Loop2.java
+++ src/com/loop/While_Loop2.java
@@ -3,11 +3,14 @@ package com.loop;
 public class While_Loop2 {
 
 	public static void main(String[] args) {
-		// 
-		int i = 1;
-		while(i <= 5) {
-		    System.out.println(i);
-		    i++;
+		// Print numbers from 10 to 1.
+		
+		int i=10;
+		
+		while(i>=1)
+		{
+			System.out.println(i);
+			i--;
 		}
 
 	}
