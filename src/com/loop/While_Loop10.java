07a73671138f6b5ebbff2f9ad65f8e93aec4d812
piyu54 <priyakushwaha1407@gmail.com>
2025-11-19 22:23:36 +0530

    commit

--- src/com/loop/While_Loop10.java
+++ src/com/loop/While_Loop10.java
@@ -3,13 +3,15 @@ package com.loop;
 public class While_Loop10 {
 
 	public static void main(String[] args) {
-		// 
-		int i = 1;
-		while(i <= 5) {
-		    System.out.println(i);
-		    i++;
+		// Print all odd numbers between 1 and 50.
+	
+		int i=1;
+		
+		while(i<=50)
+		{
+			System.out.println(i);
+			i+=2;
 		}
-
 	}
 
 }
