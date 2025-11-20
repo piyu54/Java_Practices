6134d533dc5a9bf0b309566fbab1dade4ef7e8ca
piyu54 <priyakushwaha1407@gmail.com>
2025-11-20 19:52:36 +0530

    commit

--- src/com/loop/Continue4.java
+++ src/com/loop/Continue4.java
@@ -12,14 +12,12 @@ public class Continue4 {
 //			}
 //			System.out.println(i);
 //		}
-		for(int i=1;i<=50;i++)
-		{
-			if(i%10==9)
-			{
+		for (int i = 1; i <= 50; i++) {
+			if (i % 10 == 9) {
 				System.out.println(i);
 			}
 		}
-		
+
 	}
 
 }
