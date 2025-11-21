8da92cd58ac608a59f87c493d340ce5c722febe4
piyu54 <priyakushwaha1407@gmail.com>
2025-11-21 17:39:58 +0530

    commit

---
+++ src/com/loop/Pattern.java
@@ -0,0 +1,17 @@
+package com.loop;
+
+public class Pattern {
+
+	//star pattern
+	public static void main(String[] args) {
+
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
+				System.out.println("*");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
