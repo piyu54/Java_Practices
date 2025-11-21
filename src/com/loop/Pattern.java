a370875183ade29a054651f389f31a6779dadb08
piyu54 <priyakushwaha1407@gmail.com>
2025-11-21 17:49:10 +0530

    commit

--- src/com/loop/Pattern.java
+++ src/com/loop/Pattern.java
@@ -7,7 +7,7 @@ public class Pattern {
 
 		for (int i = 1; i <= 5; i++) {
 			for (int j = 1; j <= 5; j++) {
-				System.out.println("*");
+				System.out.print("*");
 			}
 			System.out.println(" ");
 		}
