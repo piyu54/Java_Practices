c41c0b11a18945ce37d4a35db033e8e562645c9a
piyu54 <priyakushwaha1407@gmail.com>
2025-11-23 12:51:34 +0530

    commit

---
+++ src/com/loop/Pattern5.java
@@ -0,0 +1,19 @@
+package com.loop;
+
+public class Pattern5 {
+
+	
+	
+/*
+ * 22222
+ * 2222
+ * 222
+ * 22
+ * 2
+ * */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
