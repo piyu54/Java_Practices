e9d2c91f3c9e0c80b4e53420b47fa7ecb7d29ab4
piyu54 <priyakushwaha1407@gmail.com>
2025-11-23 12:51:39 +0530

    commit

---
+++ src/com/loop/Pattern6.java
@@ -0,0 +1,17 @@
+package com.loop;
+
+public class Pattern6 {
+
+	/*
+	 * 
+	 *       2
+	 *      22
+	 *     222
+	 *    2222
+	 *   22222    */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
