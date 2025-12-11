107c8817d804889c0523fb969492df82f30dc321
asus <asus@DESKTOP-R6RIO4A.bbrouter>
2025-12-11 21:19:57 +0530

    commit

---
+++ src/com/ops/inheritance/Insta.java
@@ -0,0 +1,21 @@
+package com.ops.inheritance;
+
+public class Insta extends Meta{
+
+	public void myStory()
+	{
+		System.out.println("story uploaded on insta....!");
+	}
+	
+	public void myReels()
+	{
+		System.out.println("Reels on insta.....!");
+	}
+	
+//	public static void main(String[] args) {
+//		Insta i = new Insta();
+//		i.myLogin();
+
+//	}
+
+}
