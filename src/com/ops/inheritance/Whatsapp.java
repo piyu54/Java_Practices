60c2f255343751423b266e93b4cfc179da13fd2b
asus <asus@DESKTOP-R6RIO4A.bbrouter>
2025-12-11 21:20:12 +0530

    commit

---
+++ src/com/ops/inheritance/Whatsapp.java
@@ -0,0 +1,17 @@
+package com.ops.inheritance;
+
+public class Whatsapp extends Meta {
+	
+	
+	public void myLocation()
+	{
+		System.out.println("Current Location on Whatsapp");
+	}
+
+	public void myStatus()
+	{
+		System.out.println("HBD Priya on Whatsapp");
+	}
+	
+	
+}
