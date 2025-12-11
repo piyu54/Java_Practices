1c9656ce7645510b59fc7aad7bcf8402633911c2
asus <asus@DESKTOP-R6RIO4A.bbrouter>
2025-12-11 21:20:02 +0530

    commit

---
+++ src/com/ops/inheritance/Meta.java
@@ -0,0 +1,20 @@
+package com.ops.inheritance;
+
+public class Meta {
+	
+	void myMsg()
+	{
+		System.err.println("Message functionality" );
+	}
+	
+	void myRegister()
+	{
+		System.err.println("Register Here....!");
+	}
+	
+	void myLogin()
+	{
+		System.err.println("Login Here");
+	}
+
+}
