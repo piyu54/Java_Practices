afdd46c21fb6c9e1aefee381535dbe39366f0d75
asus <priyakushwaha1407@gmail.com>
2025-12-12 20:32:26 +0530

    commit

---
+++ src/com/methods/nonparameterized/Even_Odd.java
@@ -0,0 +1,20 @@
+package com.methods.nonparameterized;
+
+public class Even_Odd {
+
+	
+	//Write a method that returns true if a number is even, else false.
+	
+	String evenOdd()
+	{
+		int num=5;
+		if(num%2==0)
+		{
+			return "even";
+		}
+		else
+		{
+			return "Odd";
+		}
+	}
+}
---
+++ src/com/methods/nonparameterized/Even_Odd2.java
@@ -0,0 +1,13 @@
+package com.methods.nonparameterized;
+
+public class Even_Odd2 {
+
+	public static void main(String[] args) {
+		
+		
+		Even_Odd e= new Even_Odd();
+		System.out.println(e.evenOdd());
+
+	}
+
+}
