4eaae8c3780caeba85523dfb4bc23948c9e24d9d
asus <priyakushwaha1407@gmail.com>
2025-12-12 16:55:54 +0530

    commit

---
+++ src/com/methods/parameterized/SumOfTwoNum.java
@@ -0,0 +1,11 @@
+package com.methods.parameterized;
+
+public class SumOfTwoNum {
+//Write a method that returns the sum of two numbers.
+	
+	
+	int sum(int a,int b)
+	{
+		return a+b;
+	}
+}
---
+++ src/com/methods/parameterized/SumOfTwoNum2.java
@@ -0,0 +1,10 @@
+package com.methods.parameterized;
+
+public class SumOfTwoNum2 {
+	public static void main(String[] args) {
+
+		
+		SumOfTwoNum s = new SumOfTwoNum();
+		System.out.println(s.sum(2, 4));
+	}
+}
