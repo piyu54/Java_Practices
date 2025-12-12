c8b018102b8fbdbf7c909b8625973057ad6e775d
asus <priyakushwaha1407@gmail.com>
2025-12-12 16:52:12 +0530

    commit

---
+++ src/com/methods/parameterized/Square.java
@@ -0,0 +1,11 @@
+package com.methods.parameterized;
+
+public class Square {
+//Write a method that returns the square of a number.
+	
+	
+	int square(int num)
+	{
+		return num*num;
+	}
+}
---
+++ src/com/methods/parameterized/Square2.java
@@ -0,0 +1,10 @@
+package com.methods.parameterized;
+
+public class Square2 {
+
+	public static void main(String[] args) {
+		
+		Square s= new Square();
+		System.out.println(s.square(5));
+	}
+}
