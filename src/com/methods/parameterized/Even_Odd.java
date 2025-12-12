6b34e4e37d46300df3bd72879eeeacb94f625b9d
asus <priyakushwaha1407@gmail.com>
2025-12-12 18:40:28 +0530

    commit

---
+++ src/com/methods/parameterized/Even_Odd.java
@@ -0,0 +1,19 @@
+package com.methods.parameterized;
+
+public class Even_Odd {
+
+	
+	//Write a method that returns true if a number is even, else false.
+	
+	String evenOdd(int num)
+	{
+		if(num%2==0)
+		{
+			return "even";
+		}
+		else
+		{
+			return "Odd";
+		}
+	}
+}
---
+++ src/com/methods/parameterized/Even_Odd2.java
@@ -0,0 +1,13 @@
+package com.methods.parameterized;
+
+public class Even_Odd2 {
+
+	public static void main(String[] args) {
+		
+		
+		Even_Odd e= new Even_Odd();
+		System.out.println(e.evenOdd(1));
+
+	}
+
+}
