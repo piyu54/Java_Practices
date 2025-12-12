e75c7f0d861c44e4c9db053b017ee78d176109ac
asus <priyakushwaha1407@gmail.com>
2025-12-12 20:34:48 +0530

    commit

---
+++ src/com/methods/nonparameterized/Square.java
@@ -0,0 +1,12 @@
+package com.methods.nonparameterized;
+
+public class Square {
+//Write a method that returns the square of a number.
+	
+	
+	int square()
+	{
+		int num=5;
+		return num*num;
+	}
+}
---
+++ src/com/methods/nonparameterized/Square2.java
@@ -0,0 +1,10 @@
+package com.methods.nonparameterized;
+
+public class Square2 {
+
+	public static void main(String[] args) {
+		
+		Square s= new Square();
+		System.out.println(s.square());
+	}
+}
