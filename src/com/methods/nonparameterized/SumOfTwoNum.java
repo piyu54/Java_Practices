757da336824190cfcad734dff4f7580b1103dcdb
asus <priyakushwaha1407@gmail.com>
2025-12-12 20:35:29 +0530

    commit

---
+++ src/com/methods/nonparameterized/SumOfTwoNum.java
@@ -0,0 +1,13 @@
+package com.methods.nonparameterized;
+
+public class SumOfTwoNum {
+//Write a method that returns the sum of two numbers.
+	
+	
+	int sum()
+	{
+		int a=6;
+		int b=9;
+		return a+b;
+	}
+}
---
+++ src/com/methods/nonparameterized/SumOfTwoNum2.java
@@ -0,0 +1,10 @@
+package com.methods.nonparameterized;
+
+public class SumOfTwoNum2 {
+	public static void main(String[] args) {
+
+		
+		SumOfTwoNum s = new SumOfTwoNum();
+		System.out.println(s.sum());
+	}
+}
