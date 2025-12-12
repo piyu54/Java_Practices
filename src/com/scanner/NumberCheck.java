12f302b2f68c3a6ffeb4339039e202452664a8ff
asus <priyakushwaha1407@gmail.com>
2025-12-12 23:14:33 +0530

    commit

--- src/com/scanner/NumberCheck.java
+++ src/com/scanner/NumberCheck.java
@@ -5,26 +5,19 @@ import java.util.Scanner;
 public class NumberCheck {
 // Take a number and check if it is positive, negative, or zero.
 	public static void main(String[] args) {
-		
+
 		Scanner s = new Scanner(System.in);
-		
+
 		System.out.println("Enter an number");
-		int num =s.nextInt();
-		
-		if(num==0)
-		{
+		int num = s.nextInt();
+
+		if (num == 0) {
 			System.out.println("num is zero");
-		}
-		else if(num>1)
-		{
+		} else if (num > 1) {
 			System.out.println("num is postive");
-		}
-		else if(num<0)
-		{
+		} else if (num < 0) {
 			System.out.println("num is negative");
-		}
-		else
-		{
+		} else {
 			System.out.println("invaild num");
 		}
 
