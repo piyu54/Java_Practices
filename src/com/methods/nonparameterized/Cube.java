782cdae61878a37aace5543e65256368f59df94f
asus <priyakushwaha1407@gmail.com>
2025-12-12 20:31:34 +0530

    commit

---
+++ src/com/methods/nonparameterized/Cube.java
@@ -0,0 +1,12 @@
+package com.methods.nonparameterized;
+
+public class Cube {
+//Write a method that returns the cube of a number.
+	
+	
+	int cube()
+	{
+		int num=6;
+		return num*num*num;
+	}
+}
---
+++ src/com/methods/nonparameterized/Cube2.java
@@ -0,0 +1,12 @@
+package com.methods.nonparameterized;
+
+public class Cube2 {
+
+	public static void main(String[] args) {
+		
+		Cube c= new Cube();
+		System.out.println(c.cube());
+
+	}
+
+}
