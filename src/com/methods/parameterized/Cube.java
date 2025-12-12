7f3484ab6e6cff0a652e7051f7b872a6faa0bff1
asus <priyakushwaha1407@gmail.com>
2025-12-12 16:52:06 +0530

    commit

---
+++ src/com/methods/parameterized/Cube.java
@@ -0,0 +1,11 @@
+package com.methods.parameterized;
+
+public class Cube {
+//Write a method that returns the cube of a number.
+	
+	
+	int cube(int num)
+	{
+		return num*num*num;
+	}
+}
---
+++ src/com/methods/parameterized/Cube2.java
@@ -0,0 +1,12 @@
+package com.methods.parameterized;
+
+public class Cube2 {
+
+	public static void main(String[] args) {
+		
+		Cube c= new Cube();
+		System.out.println(c.cube(2));
+
+	}
+
+}
