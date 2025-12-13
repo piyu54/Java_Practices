c63a02638e57c39740fe748e17f436c5b71c1113
asus <priyakushwaha1407@gmail.com>
2025-12-14 00:01:59 +0530

    commit

---
+++ src/com/encapsulation/Employeee2.java
@@ -0,0 +1,15 @@
+package com.encapsulation;
+
+public class Employeee2 {
+
+	public static void main(String[] args) {
+		
+		
+		Employeee e = new Employeee();
+		
+		e.setSalary(0);
+		System.out.println("Salary is :"+e.getSalary());
+
+	}
+
+}
