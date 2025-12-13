ae8eb362f7dd3c4e1bc2926fdb932a7f14f70435
asus <priyakushwaha1407@gmail.com>
2025-12-13 23:39:14 +0530

    commit

---
+++ src/com/encapsulation/UserAccount.java
@@ -0,0 +1,28 @@
+package com.encapsulation;
+
+public class UserAccount {
+	
+	private String username;
+	private String Password;
+	
+	
+	
+	public void setUsername(String username)
+	{
+		this.username=username;
+	}
+	public String getUsername()
+	{
+		return username;
+	}
+	
+	public void setPass(String Password)
+	{
+		this.Password=Password;
+	}
+	public String GetPass()
+	{
+		return Password;
+	}
+
+}
