eea3d9c13808cf37b14f907492915783a399a7a1
asus <priyakushwaha1407@gmail.com>
2025-12-14 18:03:06 +0530

    Commit

---
+++ src/com/constructor/Car.java
@@ -0,0 +1,32 @@
+package com.constructor;
+
+
+/*Write a program to create a class Car with:
+Non-parameterized constructor that prints “Car Started”.
+Parameterized constructor that takes model name and price.
+Create both objects.*/
+
+public class Car {
+
+	
+	private String model;
+	private String price;
+	
+	Car()
+	{
+		System.out.println("Car Started");
+	}
+	
+	Car(String model,String price)
+	{
+		this.model=model;
+		this.price=price;
+	}
+	
+	void CarInfo()
+	{
+		System.out.println("Car Information");
+		System.out.println("Car Model Name is :"+model);
+		System.out.println("Car Price is :"+price);
+	}
+}
