606a77e2d2dbea6d8b5fd11d4afd8470b753434e
asus <priyakushwaha1407@gmail.com>
2025-12-14 17:36:19 +0530

    Commit

---
+++ src/com/constructor/StudentManagement.java
@@ -0,0 +1,10 @@
+package com.constructor;
+
+public class StudentManagement {
+
+	
+	public static void main(String[] args) {
+		Student2 s = new Student2(101, "Sakshi", "25000", 1866276762, "Pune");
+		
+	}
+}
