596ef3cc54271040c33be456891dca672aec6e01
asus <priyakushwaha1407@gmail.com>
2025-12-14 18:02:55 +0530

    commit

---
+++ src/com/constructor/EmployeeDetails.java
@@ -0,0 +1,11 @@
+package com.constructor;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		
+		Employee e = new Employee(101, "Prixi","55000");
+		Employee e2= new Employee(102, "Sakshi", "25000");
+	}
+
+}
