d430cc011dda192b12586da0d3cb22078e7567d5
asus <priyakushwaha1407@gmail.com>
2025-12-16 14:03:23 +0530

    commit

---
+++ src/com/ops/types/inheritance/A3.java
@@ -0,0 +1,13 @@
+package com.ops.types.inheritance;
+/*6.Create classes A → B → C.
+Each class has one method:
+A.showA(), B.showB(), C.showC().
+Call all three using C object.*/
+public class A3 {
+
+	void showA()
+	{
+		System.out.println("This is an class A");
+	}
+	
+}
