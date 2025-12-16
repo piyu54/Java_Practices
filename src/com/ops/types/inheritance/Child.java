cd748b1ecd0025252b7069bc3bea3d5c5c097c12
asus <priyakushwaha1407@gmail.com>
2025-12-16 13:21:48 +0530

    commit

---
+++ src/com/ops/types/inheritance/Child.java
@@ -0,0 +1,17 @@
+package com.ops.types.inheritance;
+
+public class Child extends Parent{
+
+	int y=20;
+	public static void main(String[] args) {
+		
+		
+		
+		Child c = new Child();
+		System.out.println(c.x);
+		System.out.println(c.y);
+		
+
+	}
+
+}
