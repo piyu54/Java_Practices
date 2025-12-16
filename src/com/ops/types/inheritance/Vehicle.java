61f6427ac69a6d2dca999e79a59555a446ff6c7d
asus <priyakushwaha1407@gmail.com>
2025-12-16 13:25:00 +0530

    commit

---
+++ src/com/ops/types/inheritance/Vehicle.java
@@ -0,0 +1,14 @@
+package com.ops.types.inheritance;
+
+
+/*4.Create class Vehicle with method start().
+Create class Car extending Vehicle with method drive().
+Call both using child object.*/
+public class Vehicle {
+
+	
+	void start()
+	{
+		System.out.println("Car is started");
+	}
+}
