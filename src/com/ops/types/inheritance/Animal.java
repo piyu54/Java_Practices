6d0d82990de1a6cbf8ad71fb44da6ce5107d80e6
asus <priyakushwaha1407@gmail.com>
2025-12-16 13:21:43 +0530

    commit

---
+++ src/com/ops/types/inheritance/Animal.java
@@ -0,0 +1,14 @@
+package com.ops.types.inheritance;
+
+/*3.Create class Animal with method eat().
+Create class Dog extending Animal with method bark().
+Call both methods.
+*/
+public class Animal {
+
+	
+	void eat()
+	{
+		System.out.println("this is an method in animal class");
+	}
+}
