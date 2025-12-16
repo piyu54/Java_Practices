f72d4c0053a81550dfec776810f5c0eb40ab702f
asus <priyakushwaha1407@gmail.com>
2025-12-16 13:22:01 +0530

    commit

---
+++ src/com/ops/types/inheritance/Parent.java
@@ -0,0 +1,14 @@
+package com.ops.types.inheritance;
+
+
+/*
+2.Create class Parent with variable x = 10.
+Create class Child extending Parent with variable y = 20.
+Print both values using object of Child.*/
+
+public class Parent {
+
+	
+	int x=10;
+	
+}
