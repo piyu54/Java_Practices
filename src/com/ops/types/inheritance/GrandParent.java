aeea2425d9aad2d96e2c2e24f1d71a525e5fdd1c
asus <priyakushwaha1407@gmail.com>
2025-12-16 14:07:53 +0530

    commit

---
+++ src/com/ops/types/inheritance/GrandParent.java
@@ -0,0 +1,13 @@
+package com.ops.types.inheritance;
+
+/*
+7.Create classes GrandParent → Parent → Child.
+Each class has one variable:
+a=1, b=2, c=3.
+Print all values using object of Child.
+*/
+public class GrandParent {
+
+	int a=1;
+	
+}
