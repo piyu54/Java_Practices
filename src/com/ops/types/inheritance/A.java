068cc74f835e18c5b73b979b3e234eb6c6dd52b7
asus <priyakushwaha1407@gmail.com>
2025-12-16 13:15:22 +0530

    commit

---
+++ src/com/ops/types/inheritance/A.java
@@ -0,0 +1,13 @@
+package com.ops.types.inheritance;
+
+
+//1.Create class A with method showA() and class B extending A with method showB().
+//Call both methods using object of B.
+public class A {
+
+	
+	void showA()
+	{
+		System.out.println("this is an method of class A");
+	}
+}
