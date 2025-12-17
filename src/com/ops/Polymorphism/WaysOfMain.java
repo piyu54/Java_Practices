8a4bdd3d137533f7d5cb47557c0b30e4db1cb575
asus <priyakushwaha1407@gmail.com>
2025-12-17 12:06:18 +0530

    commit

---
+++ src/com/ops/Polymorphism/WaysOfMain.java
@@ -0,0 +1,21 @@
+package com.ops.Polymorphism;
+
+public class WaysOfMain {
+
+//	public static void main(String[] args) {
+//	}
+
+//	static public void main(String[] args)
+//	{
+//		
+//	}
+
+//  public	static  void main(String args[])
+//	{
+//		
+//	}
+
+	static public void main(String[] args) {
+
+	}
+}
