97b84e9315a42353802ca8682c849ea5987d7389
asus <priyakushwaha1407@gmail.com>
2025-12-17 11:49:45 +0530

    commit

---
+++ src/com/ops/Polymorphism/Methodoverloading/ReturnType.java
@@ -0,0 +1,14 @@
+package com.ops.Polymorphism.Methodoverloading;
+/*14. Write a program to show that return type alone cannot overload a method.*/
+public class ReturnType {
+
+	
+	int show(int a) {
+        return a;
+    }
+	
+//	double show(int a)
+//	{
+//		return a;
+//	}
+}
