37ca4f0dd51e5c32c0cf14e2f1a13b3b6805ac9e
asus <priyakushwaha1407@gmail.com>
2025-12-17 15:42:30 +0530

    commit

---
+++ src/com/ops/Polymorphism/NetBanking.java
@@ -0,0 +1,13 @@
+package com.ops.Polymorphism;
+
+public class NetBanking {
+
+	
+	public static void main(String[] args) {
+		
+		Banking b = new Banking();
+		b.getBanking("SBI", 436764998);
+	}
+	
+	
+}
