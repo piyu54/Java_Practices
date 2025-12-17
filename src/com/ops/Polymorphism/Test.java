3ee930b41ec97a45ec17edd52b0baebb7d6d9f70
asus <priyakushwaha1407@gmail.com>
2025-12-17 12:06:23 +0530

    commit

---
+++ src/com/ops/Polymorphism/Test.java
@@ -0,0 +1,10 @@
+package com.ops.Polymorphism;
+
+public class Test {
+
+	
+//	Can we OVERRIDE main() method?
+//			❌ NO (because main() is static)
+//
+//			Static methods belong to class, not object.
+}
