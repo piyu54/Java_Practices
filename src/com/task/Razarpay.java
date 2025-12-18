b6cd3e5d788faa76c4a04d6e469c8a8e2cb539ed
asus <priyakushwaha1407@gmail.com>
2025-12-18 18:32:17 +0530

    commit

---
+++ src/com/task/Razarpay.java
@@ -0,0 +1,14 @@
+package com.task;
+
+public class Razarpay extends Payment {
+
+	@Override
+	void reward() {
+		System.out.println("Gpay provides 15rs cashback Reward");
+	}
+	
+	@Override
+	void checkBal() {
+		System.out.println("10000 Balance");
+	}
+}
