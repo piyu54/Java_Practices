97fc3d247b09f83ff6241160106dc9423c91ea85
asus <priyakushwaha1407@gmail.com>
2025-12-18 18:41:53 +0530

    commit

---
+++ src/com/referenceexamples/TestVehicle.java
@@ -0,0 +1,19 @@
+package com.referenceexamples;
+
+public class TestVehicle {
+
+	public static void main(String[] args) {
+		
+		Vehicle v;
+		
+		v=new Car();
+		v.start();
+		
+		v=new Bike();
+		v.start();
+		
+		
+
+	}
+
+}
