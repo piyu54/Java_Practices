2f575f8265d317e7c7b88769188b4976077ab02f
asus <priyakushwaha1407@gmail.com>
2025-12-18 18:32:00 +0530

    commit

---
+++ src/com/task/Gpay.java
@@ -0,0 +1,15 @@
+package com.task;
+
+public class Gpay extends Payment{
+
+	@Override
+	void reward() {
+		System.out.println("Gpay provides 5rs cashback Reward");
+	}
+	
+	@Override
+	void checkBal() {
+		
+		System.out.println("10000 Balance");
+	}
+}
