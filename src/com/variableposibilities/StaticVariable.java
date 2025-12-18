b34893b87d5977c5147497e7846474926631d3f7
asus <priyakushwaha1407@gmail.com>
2025-12-18 17:05:07 +0530

    commit

---
+++ src/com/variableposibilities/StaticVariable.java
@@ -0,0 +1,12 @@
+package com.variableposibilities;
+
+public class StaticVariable {
+
+	
+	static int id;
+	final int id2 = 0;
+	static final String name="null";
+	
+	//this cannot be used 
+	//static variable shared among the object/
+}
