fc0984ba15f8ef78de1dfea79a90aedff9a23997
asus <priyakushwaha1407@gmail.com>
2025-12-18 18:32:13 +0530

    commit

---
+++ src/com/task/Phonepe.java
@@ -0,0 +1,14 @@
+package com.task;
+
+public class Phonepe  extends Payment{
+
+	@Override
+	void reward() {
+		System.out.println("Phonepe provides 2rs cashback Reward");
+	}
+	
+	@Override
+	void checkBal() {
+		System.out.println("10000 Balance");
+	}
+}
