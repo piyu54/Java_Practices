b9479685399e414ec1794d2612abf7d78592b802
asus <priyakushwaha1407@gmail.com>
2025-12-18 18:41:57 +0530

    commit

---
+++ src/com/referenceexamples/Vehicle.java
@@ -0,0 +1,12 @@
+package com.referenceexamples;
+
+/*Create a Vehicle parent class and Car, Bike child classes.
+Write reference creation statements to demonstrate runtime polymorphism.*/
+
+
+public class Vehicle {
+
+	void start() {
+        System.out.println("Vehicle starts");
+    }
+}
