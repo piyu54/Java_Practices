1e88a46698927e911d859195fee17e1955c2d769
asus <priyakushwaha1407@gmail.com>
2025-12-18 17:04:56 +0530

    commit

---
+++ src/com/variableposibilities/LocalVariable.java
@@ -0,0 +1,15 @@
+package com.variableposibilities;
+
+public class LocalVariable {
+
+	
+	void show()
+	{
+		int a=10;
+		final int b=20;
+		//static int c=9;
+//		this int c=9;
+//		super int c=9;
+//		void  c=9;   
+	}
+}
