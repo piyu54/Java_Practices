07e92f2ac62837db8b208c8db149247562943760
asus <priyakushwaha1407@gmail.com>
2025-12-18 17:04:53 +0530

    commit

---
+++ src/com/variableposibilities/GlobalVariable.java
@@ -0,0 +1,24 @@
+package com.variableposibilities;
+
+public class GlobalVariable {
+
+	
+	int id=10;
+	final String name="piyu";
+//	 static String address="ngp";
+	 
+//	 GlobalVariable(String address)
+//	 {
+//		 GlobalVariable.address=address;
+////		 System.out.println(address);
+//	 }
+//	 public static void main(String[] args) {
+//		
+//		System.out.println(address);
+//	}
+	GlobalVariable()
+	{
+		this.id=id;
+	}
+	
+}
