f42013a460530fec5936a774409c5805e31c25ef
asus <priyakushwaha1407@gmail.com>
2025-12-19 11:54:00 +0530

    commit

---
+++ src/com/referenceexamples/TestShape.java
@@ -0,0 +1,18 @@
+package com.referenceexamples;
+
+public class TestShape {
+
+	
+	public static void main(String[] args) {
+		
+		Shape s;
+		
+		s=new Circle();
+		s.area();
+		
+		s=new Rectangle();
+		s.area();
+	}
+	
+	
+}
