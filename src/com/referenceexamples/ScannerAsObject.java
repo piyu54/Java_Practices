f60670e3903af505eda6c99eb356efd307ee5f0d
asus <priyakushwaha1407@gmail.com>
2025-12-19 12:43:23 +0530

    commit

--- src/com/referenceexamples/ScannerAsObject.java
+++ src/com/referenceexamples/ScannerAsObject.java
@@ -2,4 +2,22 @@ package com.referenceexamples;
 
 public class ScannerAsObject {
 
+	// es ✅ we can take objects using Scanner, but not directly.
+
+// Scanner cannot read an object in one go.
+//We use Scanner to read object’s data (fields), then create the object using those values.
+
+	int id;
+	String name;
+
+	ScannerAsObject(int id, String name) {
+
+		this.id=id;
+		this.name=name;
+	}
+	
+	void display()
+	{
+		 System.out.println("Student ID is :"+id + " " +"Student Name is :"+ name);
+	}
 }
