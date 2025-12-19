6a9c1a178c977476d74bac04a02de29940e069ea
asus <priyakushwaha1407@gmail.com>
2025-12-19 11:54:20 +0530

    commit

---
+++ src/com/referenceexamples/Tier3.java
@@ -0,0 +1,11 @@
+package com.referenceexamples;
+
+public class Tier3 extends SeatArrangement{
+
+	
+	@Override
+	void getSeatAvailability(int seat) {
+		
+		System.out.println("ThirdTier "+seat);
+	}
+}
