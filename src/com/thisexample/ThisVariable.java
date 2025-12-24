b3513b7ebb59443815aa49a8a60c9cc12aeeb5fe
asus <priyakushwaha1407@gmail.com>
2025-12-24 23:13:29 +0530

    commit

---
+++ src/com/thisexample/ThisVariable.java
@@ -0,0 +1,10 @@
+package com.thisexample;
+
+//this is an example of this variable,method,constr
+public class ThisVariable {
+
+	//global variable,methods,constr but it should be in first line
+	int a=19;
+	
+	
+}
