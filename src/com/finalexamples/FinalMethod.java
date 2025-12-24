deb206236a365cfd7d49909b7a4313c353a79263
asus <priyakushwaha1407@gmail.com>
2025-12-24 23:13:17 +0530

    commit

---
+++ src/com/finalexamples/FinalMethod.java
@@ -0,0 +1,19 @@
+package com.finalexamples;
+
+public class FinalMethod {
+
+	//finalmethod we cant override that method
+	
+	
+	void m1()
+	{
+		System.out.println("Method of m1");
+	}
+	
+	final void m2()
+	{
+		System.out.println("we cannot override this  Method but we can call that method");
+	}
+	
+	
+}
