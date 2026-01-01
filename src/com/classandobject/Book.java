e3fa33e14ef1c2152f9c0418b58c148a7555cac6
asus <priyakushwaha1407@gmail.com>
2026-01-01 21:53:23 +0530

    commit

---
+++ src/com/classandobject/Book.java
@@ -0,0 +1,23 @@
+package com.classandobject;
+
+public class Book {
+
+//	Q4. Create a class Book
+//	Variables → title, author, price
+	
+	String title;
+	String author;
+	float price;
+	
+	public static void main(String[] args) {
+		
+		Book b = new Book();
+		String bookTitle=b.title="Leadership";
+		String authorName=b.author="Guru Gopal Das";
+		float bookPrice=b.price=149;
+		
+		System.out.println(bookTitle);
+		System.out.println(authorName);
+		syso
+	}
+}
