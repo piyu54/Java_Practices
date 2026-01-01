d3dbe7d786a36358eb35e3a3d40cd7ae8caec729
asus <priyakushwaha1407@gmail.com>
2026-01-01 21:49:42 +0530

    Commit

--- src/com/classandobject/Car.java
+++ src/com/classandobject/Car.java
@@ -3,4 +3,20 @@ package com.classandobject;
 public class Car {
 //Q3. Create a class Car
 //	Variables → model, price, color
+
+	String model;
+	float price;
+	String color;
+
+	public static void main(String[] args) {
+
+		Car c = new Car();
+		String CarModel = c.model = "BMW";
+		float CarPrice = c.price = 100000;
+		String CarColor = c.color = "Red";
+
+		System.out.println(CarModel);
+		System.out.println(CarPrice);
+		System.out.println(CarColor);
+	}
 }
