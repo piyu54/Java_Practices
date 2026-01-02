48e722f2712c1e329cfd61c048c1388eb0140770
asus <priyakushwaha1407@gmail.com>
2026-01-02 21:29:01 +0530

    commit

---
+++ src/com/pojo/Student.java
@@ -0,0 +1,54 @@
+package com.pojo;
+
+import java.util.Scanner;
+
+public class Student {
+
+	private int id;
+	private String name;
+	private float perc;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public float getPerc() {
+		return perc;
+	}
+	public void setPerc(float perc) {
+		this.perc = perc;
+	}
+	
+	Student(int id,String name,float perc)
+	{
+		this.id=id;
+		this.name=name;
+		this.perc=perc;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner (System.in);
+		System.out.println("Enter an student id");
+		int StuId =s.nextInt();
+		System.out.println("Enter an student name");
+		String Stuname=s.next();
+		System.out.println("Enter an student percentage");
+		float StuPer=s.nextFloat();
+		System.out.println("Student id is "+StuId);
+		System.out.println("Student name is "+Stuname);
+		System.out.println("Student name is "+StuPer);
+	}
+	
+	
+			
+}
