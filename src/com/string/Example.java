bcb8f4df2c6bcdb6f20e0df0b23942b755451507
asus <priyakushwaha1407@gmail.com>
2026-01-04 23:41:30 +0530

    commit

--- src/com/string/Example.java
+++ src/com/string/Example.java
@@ -1,6 +1,6 @@
 package com.string;
 //Print each character of a string on a new line. 
-public class Stringone {
+public class Example {
 
 	
 	public static void main(String[] args) {
@@ -18,5 +18,13 @@ public class Stringone {
 		{
 			System.out.println(name.charAt(i));
 		}
+//		
+
+//		Convert string to uppercase. 
+//		Convert string to lowercase. 
+//		Check whether two strings are equal or not. 
+//		Concatenate two strings without using + operator.
+//		Check whether a string contains the word "Java". 
+//		Find the first and last character of a string.
 	}
 }
---
+++ src/com/string/StringLen.java
@@ -0,0 +1,13 @@
+package com.string;
+
+public class StringLen {
+//		Find length of string using length() method. 
+	
+	public static void main(String[] args) {
+		
+	
+	String name="piyu";
+	
+	System.out.println(name.length());
+}
+}
