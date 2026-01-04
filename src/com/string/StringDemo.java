3da1b87e21d36c98720d8c1fd33d38708f16be2c
asus <priyakushwaha1407@gmail.com>
2026-01-04 23:25:39 +0530

    commit

--- src/com/string/StringDemo.java
+++ src/com/string/StringDemo.java
@@ -8,8 +8,8 @@ public class StringDemo {
 		
 //		String s1="java";
 //		String s2="java";
-//		System.out.println(s1==s2);//reference
-//		System.out.println(s1.equals(s2));//values
+//		System.out.println(s1==s2);//reference true
+//		System.out.println(s1.equals(s2));//values true
 		
 		
 		//different values
@@ -17,8 +17,8 @@ public class StringDemo {
 //		String s1="hello";
 //		String s2=s1;
 //		String s3="world";
-//		System.out.println(s1==s3);//reference
-//		System.out.println(s1.equals(s2));//values
+//		System.out.println(s1==s3);//reference false
+//		System.out.println(s1.equals(s2));//values true
 		
 //		
 //		String s1="hello";
@@ -28,14 +28,17 @@ public class StringDemo {
 //		System.out.println(s2);
 //		System.out.println(s1.hashCode());
 //		System.out.println(s2.hashCode());
+//		System.out.println(s1==s2);//reference false
+//		System.out.println(s1.equals(s2));//value false
+		
 		
 		//with new keyword
-		String str1 = new String("hello");
-		String str2 = "hello";
-
-		System.out.println(str1.hashCode());
-		System.out.println(str2.hashCode());
-		System.out.println(str1 == str2); // references
-		System.out.println(str1.equals(str2)); // Values
+//		String str1 = new String("hello");
+//		String str2 = "hello";
+//
+//		System.out.println(str1.hashCode());
+//		System.out.println(str2.hashCode());
+//		System.out.println(str1 == str2); // references
+//		System.out.println(str1.equals(str2)); // Values
 	}
 }
