38b0f5425f09ced03d1cfffc588ee6a595978c36
asus <priyakushwaha1407@gmail.com>
2026-01-04 23:36:05 +0530

    commit

---
+++ src/com/string/Stringone.java
@@ -0,0 +1,15 @@
+package com.string;
+//Print each character of a string on a new line. 
+public class Stringone {
+
+	
+	public static void main(String[] args) {
+		
+		String name="priya";
+		
+		for(int i=0;i<=name.length();i++)
+		{
+			System.out.println(name.charAt(i));
+		}
+	}
+}
