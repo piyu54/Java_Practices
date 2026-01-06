f355785b9bad8c63ee193cb4b7aa0e5c0babd2a2
asus <priyakushwaha1407@gmail.com>
2026-01-06 23:29:54 +0530

    commit

---
+++ src/com/pojo/PojoExample.java
@@ -0,0 +1,56 @@
+package com.pojo;
+
+public class PojoExample {
+
+	//fields
+	//arg con
+	//no arg con
+	//public getter setter
+	//tostring
+	
+	int empid;
+	String name;
+	double salary;
+	
+	public PojoExample()
+	{
+		
+	}
+	public PojoExample(int empid,String name,double salary)
+	{
+		this.empid=empid;
+		this.name=name;
+		this.salary=salary;
+		
+	}
+	
+	
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	@Override
+	public String toString() {
+		return "PojoExample [empid=" + empid + ", name=" + name + ", salary=" + salary + "]";
+	}
+	
+}
