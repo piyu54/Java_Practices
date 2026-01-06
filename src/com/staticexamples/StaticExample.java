9de345985f2645070e3ddf9e99db036d05169616
asus <priyakushwaha1407@gmail.com>
2026-01-06 23:08:57 +0530

    commit

--- src/com/staticexamples/StaticExample.java
+++ src/com/staticexamples/StaticExample.java
@@ -1,10 +1,28 @@
 package com.staticexamples;
-
+//variable,method,block,inner class
 public class StaticExample {
 
+	//variable
+	static String  clgname ="Raisoni";
+	int age;
 	
-	static String  name ="priya";
+	
+	 StaticExample(int age)
+	{
+		this.age = age;
+	}
+	 
+	 //method 
+	 
+	 public static void display()
+	 {
+		 System.out.println("static method");
+	 }
 	public static void main(String[] args) {
-		//variable,method,block,inner class
+		
+		
+		System.out.println(clgname);
+		StaticExample s = new StaticExample(11);
+		
 	}
 }
