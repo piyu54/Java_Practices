795ab9a81eecc3b4470c97d9c30fb285af8a254c
asus <priyakushwaha1407@gmail.com>
2026-01-06 23:54:04 +0530

    commit

--- src/com/pojo/Student.java
+++ src/com/pojo/Student.java
@@ -8,6 +8,19 @@ public class Student {
 	private String name;
 	private float perc;
 	
+	Student()
+	{
+		
+	}
+	
+	Student(int id,String name,float perc)
+	{
+		this.id=id;
+		this.name=name;
+		this.perc=perc;
+		
+	}
+	
 	public int getId() {
 		return id;
 	}
@@ -27,27 +40,28 @@ public class Student {
 		this.perc = perc;
 	}
 	
-	Student(int id,String name,float perc)
-	{
-		this.id=id;
-		this.name=name;
-		this.perc=perc;
-		
-	}
+
 	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", perc=" + perc + "]";
+	}
+
 	public static void main(String[] args) {
 		
-		Scanner s = new Scanner (System.in);
-		System.out.println("Enter an student id");
-		int StuId =s.nextInt();
-		System.out.println("Enter an student name");
-		String Stuname=s.next();
-		System.out.println("Enter an student percentage");
-		float StuPer=s.nextFloat();
-
-		Student st = new Student(StuId, Stuname, StuPer );
-		System.out.println(st.getId()+" "+st.getName()+" "+st.getPerc());
+		Student s= new Student(101,"piyu",92);
+		System.out.println(s);
+//		Scanner s = new Scanner (System.in);
+//		System.out.println("Enter an student id");
+//		int StuId =s.nextInt();
+//		System.out.println("Enter an student name");
+//		String Stuname=s.next();
+//		System.out.println("Enter an student percentage");
+//		float StuPer=s.nextFloat();
+//
+//		Student st = new Student(StuId, Stuname, StuPer );
+//		System.out.println(st.getId()+" "+st.getName()+" "+st.getPerc());
 	
-		s.close();
+//		s.close();
 	}		
 }
