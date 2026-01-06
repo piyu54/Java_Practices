cccd5e329abe8ac54ece9f9b60e37a52271bce0e
asus <priyakushwaha1407@gmail.com>
2026-01-06 23:12:44 +0530

    commit

--- src/com/staticexamples/StaticExample.java
+++ src/com/staticexamples/StaticExample.java
@@ -1,37 +1,37 @@
 package com.staticexamples;
+
 //variable,method,block,inner class
 public class StaticExample {
 
-	//variable
-	static String  clgname ="Raisoni";
+	// variable
+	static String clgname = "Raisoni";
 	int age;
-	
-	
-	 StaticExample(int age)
-	{
+
+	StaticExample(int age) {
 		this.age = age;
 	}
-	 
-	 //method 
-	 
-	 public static void display()
-	 {
+
+	// method
+
+	public static void display() {
 //		 static int id=101; //not possible
-		 System.out.println("static method");
-	 }
-	 
-	 
-	 //block first line to executed
-	 
-	 static 
-	 {
-		 System.out.println("static block");
-	 }
+		System.out.println("static method");
+	}
+
+	// block first line to executed no need of object creation
+
+	static {
+		System.out.println("static block");
+	}
+	// object creation
+	{
+		System.out.println("non static block");
+	}
+
 	public static void main(String[] args) {
-		
-		
+
 		System.out.println(clgname);
 		StaticExample s = new StaticExample(11);
-		
+
 	}
 }
