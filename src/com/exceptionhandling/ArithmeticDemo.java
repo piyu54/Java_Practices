8649a0ea7017521f456caa99ea1dcc3b49d28d11
asus <priyakushwaha1407@gmail.com>
2026-01-07 22:45:44 +0530

    commit

---
+++ src/com/exceptionhandling/ArithmeticDemo.java
@@ -0,0 +1,15 @@
+package com.exceptionhandling;
+
+public class ArithmeticDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("one");
+		System.out.println("two");
+		System.out.println("three");
+		System.out.println(10/0);//exception arithmeticexception execution stops 
+		System.out.println("four");
+		System.out.println("five");
+		
+	}
+}
