1d84b441024523a9548038f5db997df094e3f2d0
asus <priyakushwaha1407@gmail.com>
2026-01-07 22:45:36 +0530

    commit

---
+++ src/com/exceptionhandling/NullPointerDemo.java
@@ -0,0 +1,11 @@
+package com.exceptionhandling;
+
+public class NullPointerDemo {
+
+	public static void main(String[] args) {
+		
+		String name=null;
+		
+		System.out.println(name.length());//exception
+	}
+}
