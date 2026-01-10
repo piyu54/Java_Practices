462f08358373b5e0041addf34143b8c93e24982a
asus <priyakushwaha1407@gmail.com>
2026-01-10 23:07:57 +0530

    Commit

--- src/com/multithreading/Thread2.java
+++ src/com/multithreading/Thread2.java
@@ -1,25 +1,25 @@
 package com.multithreading;
 
-public class Thread2 implements Runnable{
+public class Thread2 implements A, B, Runnable {
 
 	@Override
 	public void run() {
-		for(int i=1;i<=100;i++)
-		{
-			System.out.println("Thread"+i);
-		try {
-			Thread.sleep(500);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+
+		for (int i = 1; i <= 50; i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		}
+
 	}
 
 	public void m1() {
-		for(int i=100;i<=199;i++)
-		{
-			System.out.println("m1 logic"+i);
+		for (int i = 50; i <= 100; i++) {
+			System.out.println(" A interface " + i);
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
@@ -28,6 +28,16 @@ public class Thread2 implements Runnable{
 			}
 		}
 	}
-	
-	
+
+	public void m2() {
+		for (int i = 110; i <= 200; i++) {
+			System.out.println("B interface " + i);
+			try {
+				Thread.sleep(1500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
