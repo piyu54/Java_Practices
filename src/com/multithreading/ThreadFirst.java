ca4e763701becab4f375dc442b72f9bf458967f8
asus <priyakushwaha1407@gmail.com>
2026-01-10 22:38:11 +0530

    commit

--- src/com/multithreading/ThreadFirst.java
+++ src/com/multithreading/ThreadFirst.java
@@ -1,5 +1,31 @@
 package com.multithreading;
 
-public class First {
+public class ThreadFirst extends Thread {
 
+	@Override
+	public void run() {
+
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	public void m1() {
+		for (int i = 110; i <= 200; i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
