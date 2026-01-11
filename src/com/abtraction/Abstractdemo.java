eef8a185a826a665ed75db191c537f67d65cb89e
asus <priyakushwaha1407@gmail.com>
2026-01-11 21:13:27 +0530

    commit

---
+++ src/com/abtraction/Abstractdemo.java
@@ -0,0 +1,13 @@
+package com.abtraction;
+
+public abstract class Abstractdemo {
+
+	
+	 abstract void m1();
+	 
+	 abstract void m3();
+	 public void m2()
+	 {
+		 System.out.println("This is an Normal method means NON Abtract Method");
+	 }
+}
