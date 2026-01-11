9a50f03a2a254facfbdaded9c9c7df199af99163
asus <priyakushwaha1407@gmail.com>
2026-01-11 23:01:47 +0530

    commit

---
+++ src/com/abtraction/Bank.java
@@ -0,0 +1,12 @@
+package com.abtraction;
+
+public interface Bank
+{
+
+	public static final String name="SBI";
+	
+	public abstract  void windraw(int ammount);
+	
+	public abstract  void checkBal();
+	
+}
