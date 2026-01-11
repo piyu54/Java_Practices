dfbfd737b35406b31118456364b7d3981f649865
asus <priyakushwaha1407@gmail.com>
2026-01-11 21:13:19 +0530

    commit

---
+++ src/com/abtraction/Demo.java
@@ -0,0 +1,17 @@
+package com.abtraction;
+
+public class Demo extends Abstractdemo{
+
+	
+	@Override
+	void m1() {
+		System.out.println("Abstract method");
+		
+	}
+	
+	@Override
+	void m3() {
+		System.out.println("Abstract method");
+		
+	}
+}
