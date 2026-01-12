44ebf0e4c4035bcd2234963d57df488c860a577f
asus <priyakushwaha1407@gmail.com>
2026-01-12 22:06:29 +0530

    commit

---
+++ src/com/array/BooleanStatic.java
@@ -0,0 +1,15 @@
+package com.array;
+
+public class BooleanStatic {
+
+	public static void main(String[] args) {
+
+		boolean[] res = { true, false, true, false };
+
+		System.out.println("Static way for Boolean");
+
+		for (int i = 0; i < res.length; i++) {
+			System.out.println(res[i]);
+		}
+	}
+}
