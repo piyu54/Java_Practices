dd7438b5d403f4a9663c5f0c17b7514ea75a799d
asus <priyakushwaha1407@gmail.com>
2026-01-12 21:52:08 +0530

    commit

--- src/com/array/Array1.java
+++ src/com/array/Array1.java
@@ -6,17 +6,22 @@ public class Array1 {
 
 	public static void main(String[] args) {
 
+		System.out.println("Array  static way");
 		int[] marks = { 10, 20, 30, 40 };
 
 		System.out.println(marks[0]);// 10
 
+		System.out.println("iteration");
 		// iteration
 		for (int i = 0; i < marks.length; i++) {
 			System.out.println(marks[i]);
 		}
 
+		System.out.println("--------------------------");
 		// dynamic way
 
+		System.out.println("Dynamic way");
+		
 		int[] marks2 = new int[5];
 		marks2[0]=10;
 		marks2[1]=20;
