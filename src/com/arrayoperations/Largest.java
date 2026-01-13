9deb8116d4d10665ce38bdce920ecc07bdf7cc4e
asus <priyakushwaha1407@gmail.com>
2026-01-13 18:24:30 +0530

    commit

---
+++ src/com/arrayoperations/Largest.java
@@ -0,0 +1,21 @@
+package com.arrayoperations;
+
+public class Largest {
+
+	// largest
+	public static void main(String[] args) {
+
+		int[] num = { 3, 4, 5, 6, 7 };
+
+		int max = num[0];
+
+		for (int i = 0; i < num.length; i++) {
+			if (num[i] > max) {
+				max += num[i];
+			}
+
+		}
+		System.out.println(max);
+
+	}
+}
