8b41447ab1b1249127db0ee68d7680775cfb2cb8
asus <priyakushwaha1407@gmail.com>
2026-01-13 17:22:28 +0530

    commit

---
+++ src/com/array/IntDynamic.java
@@ -0,0 +1,24 @@
+package com.array;
+
+public class IntDynamic {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Dynamic way for integer");
+		
+		int[] marks= new int[4];
+		marks[0]=9;
+		marks[1]=8;
+		marks[2]=9;
+		marks[3]=5;
+		
+		for(int i=0;i<marks.length;i++)
+		{
+			System.out.println(marks[i]);
+		}
+		
+		
+	}
+	
+}
