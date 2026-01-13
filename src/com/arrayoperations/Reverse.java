c284099b359edc7e808ad0f95f4b9a4484017af5
asus <priyakushwaha1407@gmail.com>
2026-01-13 18:24:25 +0530

    commit

---
+++ src/com/arrayoperations/Reverse.java
@@ -0,0 +1,20 @@
+package com.arrayoperations;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		
+
+		int[] num = new int[4];
+		num[0]=6;
+		num[1]=7;
+		num[2]=8;
+		num[3]=9;
+		
+
+		for(int i=num.length-1;i>=0;i--)
+		{
+			System.out.println(num[i]);
+		}
+	}
+}
