2b03782abb546513335dfa2e6d56654b79292dbe
asus <priyakushwaha1407@gmail.com>
2026-01-13 18:24:10 +0530

    commit

---
+++ src/com/arrayoperations/Array.java
@@ -0,0 +1,20 @@
+
+package com.arrayoperations;
+
+public class Array {
+
+	public static void main(String[] args) {
+
+		int[] num = new int[4];
+		num[0]=6;
+		num[1]=7;
+		num[2]=8;
+		num[3]=9;
+		
+
+		for(int i=0;i<num.length;i++)
+		{
+			System.out.println(num[i]);
+		}
+	}
+}
