d197601cbb6a86cc4c2dd83e6cad4a1a2a17fc2d
asus <priyakushwaha1407@gmail.com>
2026-01-16 23:19:13 +0530

    commit

--- src/com/arrayoperations1d/Palindrome.java
+++ src/com/arrayoperations1d/Palindrome.java
@@ -3,11 +3,12 @@ package com.arrayoperations1d;
 public class Palindrome {
 
 	public static void main(String[] args) {
-	int[] num= {1,2,3,2,1};
-	
-	for(int i=0;i<num.length;i++)
-	{
-		
-	}
+		int[] num = { 1, 2, 3, 2, 1 };
+
+		for (int i = 0; i < num.length; i++) {
+           
+//			if
+			
+		}
 	}
 }
