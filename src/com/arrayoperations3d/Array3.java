29d3b1dc5ee438be435cf1aee34791af482d62be
asus <priyakushwaha1407@gmail.com>
2026-01-16 23:22:31 +0530

    commit

--- src/com/arrayoperations3d/Array3.java
+++ src/com/arrayoperations3d/Array3.java
@@ -3,12 +3,10 @@ package com.arrayoperations3d;
 public class Array3 {
 	public static void main(String[] args) {
 
-		int[] num = { 1, 2, 3, 2, 1 };
+		int[][][] num = { { { 1, 2 }, { 3, 4 }, { 5, 6 } } };
 
 		for (int i = 0; i < num.length; i++) {
-           
-//			if
-			
+
 		}
 	}
 }
