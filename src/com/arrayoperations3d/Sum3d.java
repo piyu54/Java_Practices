8e2d7849c8015d9d4e10dbfffdf253da3871e254
asus <priyakushwaha1407@gmail.com>
2026-01-16 23:26:26 +0530

    commit

--- src/com/arrayoperations3d/Sum3d.java
+++ src/com/arrayoperations3d/Sum3d.java
@@ -3,6 +3,19 @@ package com.arrayoperations3d;
 public class Sum3d {
 
 	public static void main(String[] args) {
-		
+		int[][][] num = { { { 1, 2 }, { 3, 4 }, { 5, 6 } } };
+		int sum=0;
+
+		for (int i = 0; i < num.length; i++) {
+
+			for (int j = 0; j < num[i].length; j++) {
+
+				for (int k = 0; k< num[i][j].length; k++) {
+
+					sum+=num[i][j][k];
+				}
+			}
+		}
+		System.out.println(sum);
 	}
 }
