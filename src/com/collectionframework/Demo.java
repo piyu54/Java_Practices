33fd99fe0bc33564be56dc390dada44988758b29
asus <priyakushwaha1407@gmail.com>
2026-01-17 22:53:11 +0530

    commit

--- src/com/collectionframework/Demo.java
+++ src/com/collectionframework/Demo.java
@@ -1,7 +1,11 @@
 package com.collectionframework;
 
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class Demo {
 public static void main(String[] args) {
 	
+	ArrayList l = new ArrayList();
 }
 }
