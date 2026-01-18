503e8f4f58cefa836004174883fed00733834e63
asus <priyakushwaha1407@gmail.com>
2026-01-18 22:27:49 +0530

    commit

---
+++ src/com/collectionframework/ArrayListString.java
@@ -0,0 +1,23 @@
+package com.collectionframework;
+
+import java.util.ArrayList;
+
+public class ArrayListString {
+	/*Design arraylist for generics string &
+	 * iterate using for each loop
+	 */
+	public static void main(String[] args) {
+		
+		ArrayList<String> al= new ArrayList<String>();
+		al.add("piyu");
+		al.add("piu");
+		al.add("piyali");
+		al.add("priya");
+		al.add("riya");
+		
+		for(String i:al)
+		{
+			System.out.println(i);
+		}
+	}
+}
