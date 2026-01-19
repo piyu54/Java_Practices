3b2b998d4069a323656f4e8e3c52e6749b88704d
asus <priyakushwaha1407@gmail.com>
2026-01-19 21:25:46 +0530

    commit

---
+++ src/com/collectionframework/EmployeeMain.java
@@ -0,0 +1,18 @@
+package com.collectionframework;
+
+import java.util.ArrayList;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Employee> al = new ArrayList<Employee>();
+		al.add(new Employee(101,"piyu","55000"));
+		al.add(new Employee(102,"sakshi","25000"));
+		
+		for(Employee e :al)
+		{
+			System.out.println(e);
+		}
+	}
+}
