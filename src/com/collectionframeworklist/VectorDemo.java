fe5c94e5cc9e6df37a09afc5641a344f73d5318d
asus <priyakushwaha1407@gmail.com>
2026-01-19 23:21:29 +0530

    commit

--- src/com/collectionframeworklist/VectorDemo.java
+++ src/com/collectionframeworklist/VectorDemo.java
@@ -1,9 +1,11 @@
 package com.collectionframeworklist;
 
+import java.util.Vector;
+
 public class VectorDemo {
 
 	public static void main(String[] args) {
 		
-		
+		Vector v = new Vector();
 	}
 }
