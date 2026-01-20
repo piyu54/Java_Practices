7388c44f01c5842032420c65a1bda7b4ba1db18f
asus <priyakushwaha1407@gmail.com>
2026-01-20 22:44:52 +0530

    commit

---
+++ src/com/collectionframeworkset/TreeSetDemo.java
@@ -0,0 +1,25 @@
+package com.collectionframeworkset;
+
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public static void main(String[] args) {
+		
+		TreeSet ts=new TreeSet();
+		//insertion order is follow
+		//duplicate not allowed
+		// null not allowed
+		//multiple types not allowed
+		ts.add(10);
+//		ts.add("priya");
+		ts.add(20);
+//		ts.add(null);
+//		ts.add(null);
+//		ts.add("sakshi");
+//		ts.add(50);
+		
+		System.out.println(ts);
+	}
+}
