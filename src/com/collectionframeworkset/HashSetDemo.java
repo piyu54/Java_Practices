5cc435b54b20d2faebdb236a2a129c8cfb72ff01
asus <priyakushwaha1407@gmail.com>
2026-01-20 22:28:35 +0530

    commit

---
+++ src/com/collectionframeworkset/HashSetDemo.java
@@ -0,0 +1,20 @@
+package com.collectionframeworkset;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		
+		HashSet h=new HashSet();
+		h.add("piyu");
+		h.add(10);
+		h.add(null);
+		h.add("piyu");//not allowed
+		h.add(null);//not allowed
+		
+		System.out.println(h);
+
+	}
+
+}
