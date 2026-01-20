1c65c007f96a4f7f45e6a8d18a14b4dfeb9fa2df
asus <priyakushwaha1407@gmail.com>
2026-01-20 22:44:47 +0530

    commit

---
+++ src/com/collectionframeworkset/LinkedHashSetDemo.java
@@ -0,0 +1,22 @@
+package com.collectionframeworkset;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+
+	public static void main(String[] args) {
+		LinkedHashSet lhs=new LinkedHashSet();
+		//insertion order is follow
+		//duplicate not allowed
+		//multiple null not allowed
+		lhs.add(10);
+		lhs.add("priya");
+		lhs.add(null);
+		lhs.add(null);
+		lhs.add("priya");
+		lhs.add(50);
+		
+		System.out.println(lhs);
+		
+	}
+}
