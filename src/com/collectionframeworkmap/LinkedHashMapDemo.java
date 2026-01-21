8fb44fd9c43030dc5ad6dfa1735fe6f6cb1d31c3
asus <priyakushwaha1407@gmail.com>
2026-01-21 22:37:14 +0530

    commit

--- src/com/collectionframeworkmap/LinkedHashMapDemo.java
+++ src/com/collectionframeworkmap/LinkedHashMapDemo.java
@@ -1,9 +1,17 @@
 package com.collectionframeworkmap;
 
+import java.util.LinkedHashMap;
+
 public class LinkedHashMapDemo {
 	
 	public static void main(String[] args) {
-		
+		LinkedHashMap<Integer,String> lhm=new LinkedHashMap<Integer,String>();
+		lhm.put(1, "priya");
+		lhm.put(2, "sakshi");
+		lhm.put(1, "priya");
+		lhm.put(3, "piyu");
+		lhm.put(4, "piu");
+		lhm.put(4, "piu");
 		
 	}
 
