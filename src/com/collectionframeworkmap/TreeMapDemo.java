f3c0c4950b7cbd509412da9f55823e1c7ac72348
asus <priyakushwaha1407@gmail.com>
2026-01-21 22:41:37 +0530

    commit

---
+++ src/com/collectionframeworkmap/TreeMapDemo.java
@@ -0,0 +1,24 @@
+package com.collectionframeworkmap;
+
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+public class TreeMapDemo {
+
+	public static void main(String[] args) {
+		TreeMap<Integer,String> lhm=new TreeMap<Integer,String>();
+		//follow insertion order
+		//key should be unique
+		//value can be duplicate
+		//only one null accepted
+//		value can be null 
+		lhm.put(1, "priya");
+		lhm.put(2, "sakshi");
+		lhm.put(1, "riya");//value updated
+		lhm.put(3, "piyu");
+		lhm.put(4, "piu");
+		lhm.put(5, "sneha");
+		lhm.put(null, "null");
+//		lhm.put(null, "null");
+	}
+}
