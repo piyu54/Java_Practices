7183d5842f470e1acd29a92e6af7807722380a21
asus <priyakushwaha1407@gmail.com>
2026-01-21 22:40:24 +0530

    commit

--- src/com/collectionframeworkmap/LinkedHashMapDemo.java
+++ src/com/collectionframeworkmap/LinkedHashMapDemo.java
@@ -6,12 +6,19 @@ public class LinkedHashMapDemo {
 	
 	public static void main(String[] args) {
 		LinkedHashMap<Integer,String> lhm=new LinkedHashMap<Integer,String>();
+		//follow insertion order
+		//key should be unique
+		//value can be duplicate
+		//only one null accepted
+//		value can be null 
 		lhm.put(1, "priya");
 		lhm.put(2, "sakshi");
-		lhm.put(1, "priya");
+		lhm.put(1, "riya");//value updated
 		lhm.put(3, "piyu");
 		lhm.put(4, "piu");
 		lhm.put(5, "sneha");
+		lhm.put(null, "null");
+//		lhm.put(null, "null");
 		
 		System.out.println(lhm);
 		
