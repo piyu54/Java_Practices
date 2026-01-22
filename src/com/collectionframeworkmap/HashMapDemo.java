4f22a5a20e87f0ff7d6a10c8d8b42ed270af71fb
asus <priyakushwaha1407@gmail.com>
2026-01-22 12:50:24 +0530

    commit

--- src/com/collectionframeworkmap/HashMapDemo.java
+++ src/com/collectionframeworkmap/HashMapDemo.java
@@ -1,22 +1,34 @@
 package com.collectionframeworkmap;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class HashMapDemo {
 
 	public static void main(String[] args) {
 		
-		HashMap hm=new HashMap();
+		HashMap<Integer, String> hm=new HashMap<Integer, String>();
 		//insertion order not follow
-		//key-value null allow
+		//key-value null allow one only
 		//key should unique value can be same
 		//
+		
 		hm.put(10, "piyu");
+		hm.put(15, "piyu");
+		hm.put(17, "piyu");
 		hm.put(null, null);
+		hm.put(13, "piyu");
+//		hm.put(null, null);
 		hm.put(11, "piyu");
 		hm.put(12, "piyu");
-		hm.put(13, "piyu");
 		
-		System.out.println(hm);
+		
+//		System.out.println(hm);
+		
+		for (Map.Entry<Integer, String> s :hm.entrySet()) {
+			System.out.println(s.getKey() +" "+s.getValue());
+			
+		}
 	}
 }
