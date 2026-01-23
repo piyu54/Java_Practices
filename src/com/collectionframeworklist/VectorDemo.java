40e2c055de0b50693b00e1fd20b40ba548b8c54e
asus <priyakushwaha1407@gmail.com>
2026-01-23 23:40:38 +0530

    commit

--- src/com/collectionframeworklist/VectorDemo.java
+++ src/com/collectionframeworklist/VectorDemo.java
@@ -7,6 +7,8 @@ public class VectorDemo {
 	public static void main(String[] args) {
 		
 		Vector v = new Vector();
+		v.add(10);
 		System.out.println(v.contains(v));
+		System.out.println(v);
 	}
 }
