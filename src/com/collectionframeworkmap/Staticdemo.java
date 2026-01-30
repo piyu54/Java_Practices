d7036dcdefc9b24596cf921b7c782875232a1c69
asus <priyakushwaha1407@gmail.com>
2026-01-30 23:20:41 +0530

    commit

---
+++ src/com/collectionframeworkmap/Staticdemo.java
@@ -0,0 +1,20 @@
+package com.collectionframeworkmap;
+
+public class Staticdemo {
+
+static int id=10;
+	
+	static void m1()
+	{
+		System.out.println("this is an static method");
+	}
+
+
+
+	public static void main(String [] args)
+	{
+		System.out.println(Staticdemo.id);
+		Staticdemo.m1();
+		
+	}
+}
