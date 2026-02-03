83dc7763808088d76d74a2192680f719b5c36b5c
asus <priyakushwaha1407@gmail.com>
2026-02-03 22:14:04 +0530

    commit

--- src/com/collectionframeworklist/ArrayListCopy.java
+++ src/com/collectionframeworklist/ArrayListCopy.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class ArrayListCopy {
 
 	public static void main(String[] args) {
-		
+		//
 		ArrayList<Integer> al = new ArrayList<Integer>();
 		al.add(20);
 		al.add(40);
