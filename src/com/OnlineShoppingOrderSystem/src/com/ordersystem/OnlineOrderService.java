3d38f5891b16298ddea4dca5ee47e5a096cc5bba
Priya kushwaha <134228535+piyu54@users.noreply.github.com>
2026-02-06 23:56:24 +0530

    Delete src/com/OnlineShoppingOrderSystem directory

--- src/com/OnlineShoppingOrderSystem/src/com/ordersystem/OnlineOrderService.java
+++
@@ -1,9 +0,0 @@
-package com.ordersystem;
-
-public class OnlineOrderService extends OrderService{
-
-	@Override
-	public void placeOrder() {
-		 System.out.println("Order placed ONLINE");
-	}
-}
--- src/com/OnlineShoppingOrderSystem/src/com/ordersystem/StorePickupService.java
+++
@@ -1,9 +0,0 @@
-package com.ordersystem;
-
-public class StorePickupService extends OrderService {
-
-	@Override
-	public void placeOrder() {
-		System.out.println("Order placed for STORE PICKUP");
-	}
-}
