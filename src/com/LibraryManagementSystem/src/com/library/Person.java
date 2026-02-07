83caad860bffb412535b27220c13b689dc940127
Priya kushwaha <134228535+piyu54@users.noreply.github.com>
2026-02-07 23:47:21 +0530

    Add files via upload

---
+++ src/com/LibraryManagementSystem/src/com/library/Person.java
@@ -0,0 +1,27 @@
+package com.library;
+
+public class Person {
+
+	private String name;
+	private int age;
+	private String gender;
+
+	public Person(String name, int age, String gender) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+}
