3842cec0396ce793d9b6e0d7b9b55152d8fab7e5
Priya kushwaha <134228535+piyu54@users.noreply.github.com>
2026-02-08 23:48:07 +0530

    Add files via upload

---
+++ src/com/StudentManagementSystem/src/com/management/Person.java
@@ -0,0 +1,21 @@
+package com.management;
+
+public class Person {
+
+	private String name;
+	private String address;
+
+	public Person(String name, String address) {
+		this.name = name;
+		this.address = address;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+}
