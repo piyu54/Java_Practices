56d4e2ef6456e2a09b66218906bb0ab9aedae8b4
unknown <priyakushwaha1407@gmail.com>
2026-07-03 16:57:42 +0530

    commit

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -1,9 +1,53 @@
 package demo;
 
+import java.util.Scanner;
+
 public class Demo {
 
-	public static void main(String[] args) {
-		//
-		//
+	
+		public static void main(String[] args) {
+
+	        Scanner s = new Scanner(System.in);
+
+	        int arr[] = new int[10];
+
+	        System.out.println("Enter 10 numbers:");
+
+	        
+	        for (int i = 0; i < 10; i++) {
+	            arr[i] = s.nextInt();
+	        }
+
+	        System.out.print("Prime: ");
+	        for (int i = 0; i < 10; i++) {
+	            int count = 0;
+
+	            for (int j = 1; j <= arr[i]; j++) {
+	                if (arr[i] % j == 0) {
+	                    count++;
+	                }
+	            }
+
+	            if (count == 2) {
+	                System.out.print(arr[i] + " ");
+	            }
+	        }
+
+	        System.out.print("\nNot Prime: ");
+	        for (int i = 0; i < 10; i++) {
+	            int count = 0;
+
+	            for (int j = 1; j <= arr[i]; j++) {
+	                if (arr[i] % j == 0) {
+	                    count++;
+	                }
+	            }
+
+	            if (count != 2) {
+	                System.out.print(arr[i] + " ");
+	            }
+	        }
+
+	        s.close();
 	}
 }
